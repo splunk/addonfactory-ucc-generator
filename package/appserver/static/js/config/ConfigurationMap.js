3825c920a1b487b5cae592cd21593d5a88d2e95b
David Guan <wguan@splunk.com>
2016-10-10 11:38:50 +0800

    Split hardcoding out of ConfigurationPage.js

--- package/appserver/static/js/config/ConfigurationMap.js
+++ package/appserver/static/js/config/ConfigurationMap.js
@@ -1,29 +1,32 @@
-/*global define*/
-define([], function (
-) {
-    return {
-        "configuration": {
-            "header": {
-                title: "Configuration",
-                description: "Configure your Crowdstrike account, proxy and logging level.",
-                enableButton: false,
-                enableHr: false
+import ProxyView from 'app/views/Configuration/Proxy';
+import LoggingView from 'app/views/Configuration/Logging';
+import AccountView from 'app/views/Configuration/Account';
+
+export default {
+    "configuration": {
+        "header": {
+            title: "Configuration",
+            description: "Configure your Crowdstrike account, proxy and logging level.",
+            enableButton: false,
+            enableHr: false
+        },
+        "allTabs": [
+            {
+               title: "CrowdStrike Account",
+               order: 0,
+               active: true,
+               view: AccountView
             },
-            "allTabs": [
-                {
-                   title: "CrowdStrike Account",
-                   order: 0,
-                   active: true
-                },
-                {
-                    title: "Proxy",
-                    order: 1
-                },
-                {
-                    title: "Logging",
-                    order: 2
-                }
-            ]
-        }
-    };
-});
+            {
+                title: "Proxy",
+                order: 1,
+                view: ProxyView
+            },
+            {
+                title: "Logging",
+                order: 2,
+                view: LoggingView
+            }
+        ]
+    }
+};
--- package/appserver/static/js/views/Pages/ConfigurationPage.js
+++ package/appserver/static/js/views/Pages/ConfigurationPage.js
@@ -1,3 +1,5 @@
+import ConfigurationMap from 'app/config/ConfigurationMap';
+
 /*global define*/
 define([
     'jquery',
@@ -6,11 +8,7 @@ define([
     'splunkjs/mvc/headerview',
     'contrib/text!app/templates/Common/PageTitle.html',
     'contrib/text!app/templates/Common/TabTemplate.html',
-    'app/config/ConfigurationMap',
-    'app/models/Authorization',
-    'app/views/Configuration/Proxy',
-    'app/views/Configuration/Logging',
-    'app/views/Configuration/Account'
+    'app/models/Authorization'
 ], function (
     $,
     _,
@@ -18,44 +16,27 @@ define([
     HeaderView,
     PageTitleTemplate,
     TabTemplate,
-    ConfigurationMap,
-    Authorization,
-    ProxyView,
-    LoggingView,
-    AccountView
+    Authorization
 ) {
     return Backbone.View.extend({
-        initialize: function () {
+        initialize: function() {
             var configuration_template_data = ConfigurationMap.configuration.header,
                 title_template = _.template(PageTitleTemplate),
                 tab_title_template = '<li <% if (active) { %> class="active" <% } %>><a href="#<%= token%>" id="<%= token%>-li"><%= title%></a></li>',
                 tab_content_template = '<div id="<%= token%>-tab" class="tab-pane <% if (active){ %>active<% } %>"></div>',
-                // tabs = ConfigurationMap.configuration.tabs,
-                renderMap = {
-                    'proxy': this.renderProxy.bind(this),
-                    'logging': this.renderLogging.bind(this),
-                    'crowdstrike-account': this.renderServer.bind(this),
-                    'template': this.renderTemplate.bind(this)
-                },
                 self = this;
 
             $(".addonContainer").append(title_template(configuration_template_data));
             $(".addonContainer").append(_.template(TabTemplate));
 
-
-            this.proxyRendered = false;
-            this.loggingRendered = false;
-            this.serverRendered = false;
-            this.templateRendered = false;
-
-
-            function renderTabs(tabs){
+            function renderTabs(tabs) {
                 _.each(tabs, function (tab) {
                     if (tab.title === "Forwarders" && self.show_fowarder === "0") {
                         return;
                     }
                     var title = tab.title,
                         token = title.toLowerCase().replace(/\s/g, '-'),
+                        view = new tab.view(),
                         active;
 
                     if (!self.tabName) {
@@ -64,10 +45,9 @@ define([
                         active = true;
                     }
 
-                    $(".nav-tabs").append(_.template(tab_title_template, {title: title, token: token, active: active}));
-                    $(".tab-content").append(_.template(tab_content_template, {token: token, active: active}));
-                    //render each tab content
-                    renderMap[token]();
+                    $(".nav-tabs").append(_.template(tab_title_template, {title, token, active}));
+                    $(".tab-content").append(_.template(tab_content_template, {token, active}));
+                    $(`#${token}-tab`).html(view.render().$el);
                 });
             }
             var tabs = ConfigurationMap.configuration.allTabs;
@@ -79,9 +59,7 @@ define([
                     '*filter': 'changeTab'
                 },
                 changeTab: function (params) {
-                    if (params === null) {
-                        return;
-                    }
+                    if (params === null) return;
 
                     self.tabName = params;
                     $('.nav-tabs li').removeClass('active');
@@ -92,37 +70,6 @@ define([
             });
             var router = new Router();
             Backbone.history.start();
-        },
-
-        renderProxy: function () {
-            if (!this.proxyRendered) {
-                var proxy_view = new ProxyView();
-                $("#proxy-tab").append(proxy_view.render().$el);
-                this.proxyRendered = true;
-            }
-        },
-
-        renderLogging: function () {
-            if (!this.loggingRendered) {
-                var logging_view = new LoggingView();
-                $('#logging-tab').html(logging_view.render().$el);
-                this.loggingRendered = true;
-            }
-        },
-
-        renderServer: function () {
-            if (!this.serverRendered) {
-                var server_view = new AccountView();
-                $('#crowdstrike-account-tab').html(server_view.render().$el);
-                this.serverRendered = true;
-            }
-        },
-        renderTemplate: function () {
-            if (!this.templateRendered) {
-                var template_view = new TemplateView();
-                $('#template-tab').html(template_view.render().$el);
-                this.templateRendered = true;
-            }
         }
     });
 });
