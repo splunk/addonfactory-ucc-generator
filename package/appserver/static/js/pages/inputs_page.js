23ed81a5319695474c47b319293f385439fbf3a5
David Guan <wguan@splunk.com>
2016-10-17 16:25:38 +0800

    Fetch external config file dynamically.

--- package/appserver/static/js/pages/inputs_page.js
+++ package/appserver/static/js/pages/inputs_page.js
@@ -4,32 +4,41 @@ require([
     'backbone',
     'splunkjs/mvc/headerview',
     'app/views/pages/InputsPage',
-    'app/util/webpack'
+    'app/util/webpack',
+    'app/util/script'
 ], function (
     $,
     _,
     Backbone,
     HeaderView,
     InputsPageView,
-    {generatePublicPath}
+    {generatePublicPath},
+    {loadGlobalConfig}
 ) {
-    var globalConfig = null;
-    if (__CONFIG_FROM_FILE__) {
-        globalConfig = require('app/config/GlobalConfig');
-    } else {
-        globalConfig = window.globalConfig;
+    let globalConfig;
+    const render = () => {
+        const appName = globalConfig && globalConfig.name;
+        __webpack_public_path__ = generatePublicPath(appName || 'Splunk_TA_crowdstrike');
+        new HeaderView({
+            id: 'header',
+            section: 'dashboards',
+            el: $('.preload'),
+            acceleratedAppNav: true
+        }).render();
+        // Set the title
+        document.title = 'Inputs';
+        var inputsPageView = new InputsPageView();
+        inputsPageView.render();
+        $(".addonContainer").html(inputsPageView.el);
     }
-    const appName = globalConfig && globalConfig.name;
-    __webpack_public_path__ = generatePublicPath(appName || 'Splunk_TA_crowdstrike');
-    new HeaderView({
-        id: 'header',
-        section: 'dashboards',
-        el: $('.preload'),
-        acceleratedAppNav: true
-    }).render();
-    // Set the title
-    document.title = 'Inputs';
-    var inputsPageView = new InputsPageView();
-    inputsPageView.render();
-    $(".addonContainer").html(inputsPageView.el);
+
+    if (__CONFIG_FROM_FILE__) {
+        globalConfig = require('app/config/globalConfig');
+        render();
+    } else {
+        loadGlobalConfig(() => {
+            globalConfig = window.globalConfig;
+            render();
+        })
+    }
 });
---
+++ package/appserver/static/js/util/script.js
@@ -0,0 +1,31 @@
+export function loadGlobalConfig(callback)
+{
+    // Adding the script tag to the head as suggested before
+    const head = document.getElementsByTagName('head')[0];
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = `${getBuildDirPath()}/globalConfig.js`;
+
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    // Fire the loading
+    head.appendChild(script);
+}
+
+export function getBuildDirPath() {
+    const scripts = document.getElementsByTagName("script");
+
+    const scriptsCount = scripts.length;
+    for (let i = 0; i < scriptsCount; i++) {
+        const s = scripts[i];
+        if(s.src && s.src.match(/js\/build/)) {
+            const lastSlashIndex = s.src.lastIndexOf('/');
+            return s.src.slice(0, lastSlashIndex);
+        }
+    }
+
+    return '';
+}
