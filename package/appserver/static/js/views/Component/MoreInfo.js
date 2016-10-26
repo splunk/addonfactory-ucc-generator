d58d4e07746682c01b3fb3e1d9d0b0582af1c2da
David Guan <DavidGuanDev@Gmail.com>
2016-10-26 17:20:19 +0800

    Refactor MoreInfo.

--- package/appserver/static/js/views/Component/MoreInfo.js
+++ package/appserver/static/js/views/Component/MoreInfo.js
@@ -12,30 +12,31 @@ define([
 
         initialize: function (options) {
             BaseView.prototype.initialize.apply(this, arguments);
-            this.$el.addClass((this.options.index % 2) ? 'even' : 'odd').css('display', 'none');
-            this.component = options.model.component;
-            this.model = options.model.entity;
+            this.$el.addClass((options.index % 2) ? 'even' : 'odd')
+                .css('display', 'none');
         },
 
         render: function () {
-            var header = this.component.table.header,
-                moreInfo = this.component.table.moreInfo;
+            // TOOD: Component shouldn't care about how these data are retrived.
+            const {header, moreInfo} = this.options.model.component.table;
+            const {entry} = this.options.model.entity;
 
             this.$el.html(_.template(this.template, {cols: header.length + 1}));
+
             _.each(moreInfo, m => {
-                if (this.model.entry.content.attributes.hasOwnProperty(m.field)) {
-                    this.$('.list-dotted').append('<dt>' + m.label + '</dt><dd>' +
-                        (m.mapping ? m.mapping(this.model.entry.content.attributes[m.field]) : this.model.entry.content.attributes[m.field]) +
-                        '</dd>');
-                } else if (this.model.entry.hasOwnProperty(m.field)){
-                    this.$('.list-dotted').append('<dt>' + m.label + '</dt><dd>' +
-                        (m.mapping ? m.mapping(this.model.entry.attributes[m.field]) : this.model.entry.attributes[m.field]) +
-                        '</dd>');
-                }
+                const value = entry.content.attributes[m.field] || entry.attributes[m.field];
+                this.$('.list-dotted').append(`
+                    <dt>${m.label}</dt>
+                    <dd>${m.mapping ? m.mapping(value) : value}</dd>
+                `)
             });
             return this;
         },
 
-        template: '<td class="details" colspan="<%= cols %>"><dl class="list-dotted"></dl></td>'
+        template: `
+            <td class="details" colspan="<%= cols %>">
+                <dl class="list-dotted"></dl>
+            </td>
+        `
     });
 });
