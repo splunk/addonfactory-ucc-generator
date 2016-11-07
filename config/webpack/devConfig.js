eb119cd4730bacafdd8d4063cac9216819c35bd1
David Guan <DavidGuanDev@Gmail.com>
2016-11-07 14:39:23 +0800

    Add OldWatchingPlugin for dev environment.

--- config/webpack/devConfig.js
+++ config/webpack/devConfig.js
@@ -8,7 +8,10 @@ module.exports = {
         new webpack.optimize.CommonsChunkPlugin("common.js"),
         new webpack.DefinePlugin({
             __CONFIG_FROM_FILE__: false
-        })
+        }),
+        // Use old watching plugin as the bug below
+        // https://github.com/webpack/webpack/issues/675#issuecomment-224991459
+        new webpack.OldWatchingPlugin()
     ],
     watch: true,
     keepalive: true
