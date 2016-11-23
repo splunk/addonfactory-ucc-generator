ca7a93ec45bec1439ad3770891802ccc36802883
Lynch Wu <lwu@splunk.com>
2016-11-23 17:05:42 +0800

    Change rest mapping

--- package/appserver/static/js/constants/restEndpointMap.js
+++ package/appserver/static/js/constants/restEndpointMap.js
@@ -1,7 +1,7 @@
 export default {
-    "account": "configs/conf-crowdstrike_account",
-    "proxy": "configs/conf-crowdstrike",
-    "logging": "configs/conf-crowdstrike",
-    "crowdstrike_input_1": "configs/conf-crowdstrike_input_1",
-    "crowdstrike_input_2": "configs/conf-crowdstrike_input_1"
+    "account": "configs/conf-account",
+    "proxy": "configs/conf-example",
+    "logging": "configs/conf-example",
+    "inputs_01": "configs/conf-input_01",
+    "inputs_02": "configs/conf-input_02"
 }
