ebb1761d093080db631d4fa0d4c17159eeafb8bb
David Guan <DavidGuanDev@Gmail.com>
2016-11-28 10:42:02 +0800

    Restructure CustomizedTabView

--- package/appserver/static/js/views/Configuration/CustomizedTabView.js
+++ package/appserver/static/js/views/Configuration/CustomizedTabView.js
@@ -4,34 +4,55 @@ define([
     'jquery',
     'lodash',
     'backbone',
+    'app/views/configuration/NormalTabViewTemplate.html',
     'app/views/controls/ControlWrapper'
 ], function (
     $,
     _,
     Backbone,
+    NormalTabViewTemplate,
     ControlWrapper
 ) {
     return Backbone.View.extend({
-        initialize: function (options) {
+        initialize: function(options) {
+            this.isTableBasedView = !!options.props.table;
             this.props = options.props;
+
+            this.initDataBinding();
         },
 
-        render: function() {
-            const {entity} = this.props;
+        initDataBinding: function() {
 
+        },
+
+        renderNormalView: function() {
+            this.$el.html(_.template(NormalTabViewTemplate));
+
+            const {entity} = this.props;
             entity.forEach(d => {
                 const controlOptions = {
+                    ...d.options,
+                    model: this.model,
                     modelAttribute: d.field,
                     password: d.encrypted ? true : false
                 };
                 _.extend(controlOptions, d.options);
                 const controlWrapper = new ControlWrapper({...d, controlOptions});
-                this.$el.append(controlWrapper.render().$el);
+                this.$('.modal-body').append(controlWrapper.render().$el);
             });
-            // TODO change below to button control
-            this.$el.append(`
-                <input type="submit" class="btn btn-primary submit-btn" value="Save" style="margin-left: 170px">
-            `);
+        },
+
+        renderTableBasedView: function() {
+
+        },
+
+        render: function() {
+            if (this.isTableBasedView) {
+                this.renderTableBasedView();
+            } else {
+                this.renderNormalView();
+            }
+
             return this;
         }
     });
