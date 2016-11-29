e4098cc17769d3e371f12ce3d282ad14bfd1f4b8
Lynch Wu <lwu@splunk.com>
2016-11-29 10:47:04 +0800

    Remove hard code part

--- UCC-example-addon/build.py
+++ UCC-example-addon/build.py
@@ -3,11 +3,71 @@ import errno
 import json
 import os
 import subprocess
+from StringIO import StringIO
 
 
 from splunktaucclib.rest_handler.admin_external import AdminExternalHandler
 from uccrestbuilder.global_config import GlobalConfigSchema
 from uccrestbuilder import build
+
+_input_template = '''
+import sys
+import json
+
+from splunklib import modularinput as smi
+
+
+class {class_name}(smi.Script):
+
+    def __init__(self):
+        super({class_name}, self).__init__()
+
+    def get_scheme(self):
+        scheme = smi.Scheme('{input_name}')
+        scheme.description = '{description}'
+        scheme.use_external_validation = True
+        scheme.streaming_mode_xml = True
+        scheme.use_single_instance = True
+
+        scheme.add_argument(
+            smi.Argument(
+                'name',
+                title='Name',
+                description='Name',
+                required_on_create=True
+            )
+        )
+        {argument_list}
+        return scheme
+
+    def validate_input(self, definition):
+        return
+
+    def stream_events(self, inputs, ew):
+        input_items = [{{'count': len(inputs.inputs)}}]
+        for input_name, input_item in inputs.inputs.iteritems():
+            input_item['name'] = input_name
+            input_items.append(input_item)
+        event = smi.Event(
+            data=json.dumps(input_items),
+            sourcetype='{input_name}',
+        )
+        ew.write_event(event)
+
+if __name__ == '__main__':
+    exit_code = {class_name}().run(sys.argv)
+    sys.exit(exit_code)
+'''
+
+_argument_template = '''
+        scheme.add_argument(
+            smi.Argument(
+                '{field}',
+                required_on_create={required},
+            )
+        )
+'''
+
 
 basedir = os.path.dirname(os.path.abspath(__file__))
 
@@ -41,6 +101,26 @@ def copy_directory(src, dest):
             shutil.copy(src, dest)
         else:
             print'Directory %s not copied. Error: %s' % (src, exc)
+
+
+def indent(lines, spaces=1):
+    """
+    Indent code block.
+
+    :param lines:
+    :type lines: str
+    :param spaces: times of four
+    :return:
+    """
+    string_io = StringIO(lines)
+    indentation = spaces * 4
+    prefix = ' ' * indentation
+    lines = []
+    for line in string_io:
+        if line != '\n':
+            line = prefix + line
+        lines.append(line)
+    return ''.join(lines)
 
 
 def generate_ui():
@@ -95,14 +175,40 @@ def copy_res():
         basedir + "/res/app.conf",
         basedir + "/output/" + schema_content.get("meta").get("name") + "/default"
     )
-    shutil.copy(
-        basedir + "/res/inputs_01.py",
-        basedir + "/output/" + schema_content.get("meta").get("name") + "/bin"
-    )
-    shutil.copy(
-        basedir + "/res/inputs_02.py",
-        basedir + "/output/" + schema_content.get("meta").get("name") + "/bin"
-    )
+
+
+def add_modular_input():
+    services = schema_content.get("pages").get("inputs").get("services")
+    for service in services:
+        input_name = service.get("name")
+        class_name = input_name.upper()
+        description = service.get("title")
+        entity = service.get("entity")
+        argument_list = []
+        field_white_list = ["name", "index"]
+        for ent in entity:
+            if ent.get("field") in field_white_list:
+                continue
+            if ent.get("required"):
+                argument_list.append(_argument_template.format(
+                    field=ent.get("field"),
+                    required=True
+                ))
+            else:
+                argument_list.append(_argument_template.format(
+                    field=ent.get("field"),
+                    required=False
+                ))
+        argument_lines = ''.join(argument_list)
+        content = _input_template.format(
+            input_name=input_name,
+            class_name=class_name,
+            description=description,
+            argument_list=indent(argument_lines, spaces=0)
+        )
+        input_file_name = basedir + "/output/" + schema_content.get("meta").get("name") + "/bin/" + input_name + ".py"
+        with open(input_file_name, "w") as input_file:
+            input_file.write(content)
 
 
 def copy_global_config():
@@ -118,3 +224,4 @@ copy_libs()
 replace_token()
 copy_res()
 copy_global_config()
+add_modular_input()
