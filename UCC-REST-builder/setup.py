e5a2fffb1529f72db7c31f5205bd4f53937bf5aa
Minghe <mhuang@splunk.com>
2016-12-07 16:42:24 +0800

    setup publish

--- UCC-REST-builder/setup.py
+++ UCC-REST-builder/setup.py
@@ -1,28 +1,16 @@
-
 from setuptools import setup, find_packages
-
 
 setup(
     name='uccrestbuilder',
-
     description='The REST builder tool for Universal Configuration Console',
-
     version=3.0,
-
     author='Splunk, Inc.',
-
     author_email='Shanghai-TA-dev@splunk.com',
-
     license='http://www.apache.org/licenses/LICENSE-2.0',
-
     url='https://git.splunk.com/scm/soln/ta-ui-framework.git',
-
     packages=find_packages(exclude=['tests', 'examples']),
-
     package_data={'': ['LICENSE']},
-
     install_requires=[],
-
     classifiers=[
         'Programming Language :: Python',
         'Development Status :: 1 - Alpha',
