8d6f81f526602e839b873349e578b2d3339ff384
Lynch Wu <lwu@splunk.com>
2016-12-13 09:20:55 +0800

    Change error/warning/loading message style and refactor related code.

--- UCC-UI-lib/package/appserver/static/js/views/component/DeleteDialog.js
+++ UCC-UI-lib/package/appserver/static/js/views/component/DeleteDialog.js
@@ -1,18 +1,19 @@
-/*global define*/
+import {
+    addErrorMsg,
+    removeWarningMsg,
+    addClickListener
+} from 'app/util/promptMsgController';
+
 define([
     'jquery',
     'lodash',
     'backbone',
-    'app/templates/dialogs/DeleteDialog.html',
-    'app/templates/messages/ErrorMsg.html',
-    'app/templates/messages/WarningMsg.html'
+    'app/templates/dialogs/DeleteDialog.html'
 ], function (
     $,
     _,
     Backbone,
-    DeleteInput,
-    ErrorMsg,
-    WarningMsg
+    DeleteInput
 ) {
     return Backbone.View.extend({
         template: _.template(DeleteInput),
@@ -60,50 +61,6 @@ define([
             this._delete(delete_url);
         },
 
-        removeErrorMsg: function () {
-            if (this.$('.msg-error').length) {
-                this.$('.msg-error').remove();
-            }
-        },
-
-        removeWarningMsg: function () {
-            if (this.$('.msg-warning').length) {
-                this.$('.msg-warning').remove();
-            }
-        },
-
-        parseAjaxError: function (model) {
-            var rsp = JSON.parse(model.responseText),
-                regx = /In handler.+and output:\s+\'([\s\S]*)\'\.\s+See splunkd\.log for stderr output\./,
-                msg = String(rsp.messages[0].text),
-                matches = regx.exec(msg);
-            if (!matches || !matches[1]) {
-                // try to extract another one
-                regx = /In handler[^:]+:\s+(.*)/;
-                matches = regx.exec(msg);
-                if (!matches || !matches[1]) {
-                    return msg;
-                }
-            }
-            return matches[1];
-        },
-
-        addErrorMsg: function (text) {
-            if (this.$('.msg-error').length) {
-                this.$('.msg-error > .msg-text').text(text);
-            } else {
-                this.$(".modal-body").prepend(_.template(ErrorMsg)({msg: text}));
-            }
-        },
-
-        addWarningMsg: function (text) {
-            if (this.$('.msg-error').length) {
-                this.$('.msg-error > .msg-text').text(text);
-            } else {
-                this.$(".modal-body").prepend(_.template(WarningMsg)({msg: text}));
-            }
-        },
-
         modal: function () {
             this.$("[role=dialog]").modal({backdrop: 'static', keyboard: false});
         },
@@ -145,9 +102,10 @@ define([
                     this.dispatcher.trigger('delete-input');
                 }
                 this.$("[role=dialog]").modal('hide');
-            }.bind(this)).fail(function (model) {
-                this.removeWarningMsg();
-                this.addErrorMsg(this.parseAjaxError(model));
+            }.bind(this)).fail(function (model, response) {
+                removeWarningMsg('.modal-content');
+                addErrorMsg('.modal-content', response, true);
+                addClickListener('.modal-content', 'msg-error');
             }.bind(this));
         }
     });
