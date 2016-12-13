4c82cc3799440a79cce220cc2298e7c482b75adb
David Guan <DavidGuanDev@Gmail.com>
2016-12-13 11:29:30 +0800

    Support ISO8601 checks in front-end.

--- UCC-UI-lib/testes/constants/preDefinedRegex-test.js
+++ UCC-UI-lib/testes/constants/preDefinedRegex-test.js
@@ -16,7 +16,11 @@ describe('Predefined regex testes', () => {
         });
 
         it('A url with ftp protocol should match regex', () => {
-            assert.equal(true, REGEX_URL.test('ftp://www.splunk.com/:800'));
+            assert.equal(true, REGEX_URL.test('ftp://www.splunk.com/:8800'));
+        });
+
+        it('A url with for opc.tcp protocol should match regex', () => {
+            assert.equal(true, REGEX_URL.test('opc.tcp://www.splunk.com/:800'));
         });
 
         it('A url with opc.tcp protocol should match regex', () => {
@@ -48,13 +52,17 @@ describe('Predefined regex testes', () => {
         });
     });
 
-    describe('Regex for date should works', () => {
-        it('A normal date should match regex', () => {
-            assert.equal(true, REGEX_DATE.test('29/3/2015'));
+    describe('Regex for date should be valid for ISO8601 date string', () => {
+        it('A normal ISO8601 date should match regex', () => {
+            assert.equal(true, REGEX_DATE.test('2016-07-16'));
         });
 
-        it('A wrong date should not match regex', () => {
-            assert.equal(false, REGEX_DATE.test('40/3/2015'));
+        it('A wrong ISO8601 date should not match regex', () => {
+            assert.equal(false, REGEX_DATE.test('2015-3-19'));
+        });
+
+        it('Date with YYYY-MM-DDThh:mm:ss.sTZD format should works', () => {
+            assert.equal(true, REGEX_DATE.test('1997-07-16T19:20:30.45+01:00'));
         });
     });
 });
