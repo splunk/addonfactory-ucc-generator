df059e808d026714445943f287df04f79fe525d8
Lynch Wu <lwu@splunk.com>
2017-03-07 11:24:11 +0800

    Adopt router and delete deprecated headerview for ADDON-12859.

---
+++ UCC-UI-lib/package/appserver/static/js/router/PageRouter.js
@@ -0,0 +1,59 @@
+define([
+    'lodash',
+    'jquery',
+    'backbone',
+    'routers/Base',
+    'uri/route',
+    'app/views/pages/InputsPage',
+    'app/views/pages/ConfigurationPage'
+], function (
+    _,
+    $,
+    Backbone,
+    BaseRouter,
+    route,
+    InputsPageView,
+    ConfigurationPageView
+) {
+    return BaseRouter.extend({
+        routes: {
+            ':locale/app/:app/:page(/)': '_route',
+            '*root/:locale/app/:app/:page(/)': '_routeRooted'
+        },
+        initialize: function () {
+            BaseRouter.prototype.initialize.apply(this, arguments);
+            // flag that indicate whether header has been rendered
+            this._headerReady = false;
+        },
+
+        _renderHeader: function() {
+            $('.preload').replaceWith(this.pageView.el);
+        },
+        /*
+         THE ENTRY POINT
+         */
+        _route: function (locale, app, page, queryString) {
+            var args = arguments;
+            BaseRouter.prototype.page.apply(this, args);
+            this.deferreds.pageViewRendered.done(() => {
+                if (!this._headerReady) {
+                    this._renderHeader();
+                    this._headerReady = true;
+                }
+                if (page === 'inputs') {
+                    const inputsPageView = new InputsPageView();
+                    inputsPageView.render();
+                    $(".main-section-body").html(inputsPageView.el);
+                } else if (page === 'configuration') {
+                    const configurationPageView = new ConfigurationPageView();
+                    configurationPageView.render();
+                    $(".main-section-body").html(configurationPageView.el);
+                    configurationPageView.changeTab(queryString);
+                }
+            });
+        },
+        _routeRooted: function (root, locale, app, page, queryString) {
+            this._route(locale, app, page, queryString);
+        },
+    });
+});
