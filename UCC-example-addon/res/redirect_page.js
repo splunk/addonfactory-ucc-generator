013514fec167b45e08ce7a0e77546882753cc6ca
Bhargav Nariyani (C) <bnariyani@splunk.com>
2018-07-14 13:18:15 +0530

    ADDON-18559 PR review comments, formated redirect_page.js

--- UCC-example-addon/res/redirect_page.js
+++ UCC-example-addon/res/redirect_page.js
@@ -1,39 +1,30 @@
 /*global require,document*/
-require([], function()
-{
-	
+require([], function() {
     // This method takes param name and searches it from windows url, if found returns it
-
-    var getUrlParam = function(param) 
-	{
+    var getUrlParam = function(param) {
         var params = window.location.search.substring(1);
         params = params.split("&");
-        for(var i = 0; i < params.length; i++) 
-		{
+        for (var i = 0; i < params.length; i++) {
             var kv = params[i].split("=");
-            if(kv[0] == param) 
-			{
+            if(kv[0] === param) {
                 return kv[1];
             }
         }
     };
-    //Check if we get any error param in url
-    var error = getUrlParam("error");
-    var message = {};
-    //If we get error param return the error param
-    if(error !== undefined)
-	{
+    // Check if we get any error param in url
+    var error = getUrlParam("error"),
+        message = {};
+    // If we get error param return the error param
+    if (error !== undefined) {
         message = {"error":error};
-    }
-	else
-	{
-        //else return the code and state param
-        var code = getUrlParam("code");
-        var state = getUrlParam("state");
+    } else {
+        // Else return the code and state param
+        var code = getUrlParam("code"),
+            state = getUrlParam("state");
         message = {"code": code, "state": state};
     }
-    //Call the parent windows' getMessage method
+    // Call the parent windows' getMessage method
     window.opener.getMessage(message);
-    //Close the window
+    // Close the window
     window.close();
 });
