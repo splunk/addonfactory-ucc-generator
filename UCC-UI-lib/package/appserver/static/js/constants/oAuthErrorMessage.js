09fdda8d61df1c524838d66686f1975fc572e350
Dipen Adroja (C) <dadroja@splunk.com>
2018-08-09 17:24:52 +0530

    Updated error message

--- UCC-UI-lib/package/appserver/static/js/constants/oAuthErrorMessage.js
+++ UCC-UI-lib/package/appserver/static/js/constants/oAuthErrorMessage.js
@@ -1,4 +1,4 @@
 export const ERROR_REQUEST_TIMEOUT_TRY_AGAIN = 'Request time out while authenticating. Please try again.';
 export const ERROR_REQUEST_TIMEOUT_ACCESS_TOKEN_TRY_AGAIN = 'Request time out while getting accesstoken. Please try again.';
 export const ERROR_OCCURRED_TRY_AGAIN = "Error occurred while trying to authenticate. Please try Again.";
-export const ERROR_AUTH_PROCESS_TERMINATED_TRY_AGAIN = "Authentication process terminated manually. Please try again.";
+export const ERROR_AUTH_PROCESS_TERMINATED_TRY_AGAIN = "Authentication process interrupted. Please try again.";
