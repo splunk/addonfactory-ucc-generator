2e950d761a7e3fa7e197314dfbf412b57a3da5eb
Kamlesh Vaghela (C) <kvaghela@splunk.com>
2018-12-06 18:17:03 +0530

    ADDON-20253: Added comment on event

--- UCC-UI-lib/package/appserver/static/js/views/pages/ConfigurationPage.js
+++ UCC-UI-lib/package/appserver/static/js/views/pages/ConfigurationPage.js
@@ -68,6 +68,11 @@ define([
             }
         },
 
+        /**
+         * Method to activate the tab based on params value or URL Query Parameters e.g. ..../pageName?tab=mytab
+         * If tab input name is incorrect, it will open the first tab by default.
+         * @param params = Tab Name
+         */
         _activateTab(params){
             this.tabName = params;
             $('.nav-tabs li').removeClass('active');
