0038f591dd232ddbb26c008fc91e163e28c2f9dc
Mitika Pandya (C) <mpandya@splunk.com>
2019-07-19 09:10:43 +0530

    ADDON-22198: Fixed 'select' method in Dropdown

--- test/ui/Base_UccLib_3/components/dropdown.py
+++ test/ui/Base_UccLib_3/components/dropdown.py
@@ -67,6 +67,7 @@ class Dropdown(BaseComponent):
 
     def select(self, value):
 
+        self.add_btn.click()
         for each in self.get_elements('type_list'):
             if each.text.strip().lower() == value.lower():
                 each.click()
