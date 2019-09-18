a15b5e61e838bfaaa2abcaa65a07df65512befe5
Naqeebali Shamsi (C) <nshamsi@splunk.com>
2019-09-18 15:23:18 +0530

    final fix

--- UCC-REST-lib/httplib2_helper/httplib2_py3/httplib2/socks.py
+++ UCC-REST-lib/httplib2_helper/httplib2_py3/httplib2/socks.py
@@ -267,13 +267,13 @@ class socksocket(socket.socket):
         elif chosenauth[1:2] == chr(0x02).encode():
             # Okay, we need to perform a basic username/password
             # authentication.
-            self.sendall(
-                chr(0x01).encode()
-                + chr(len(self.__proxy[4]))
-                + self.__proxy[4]
-                + chr(len(self.__proxy[5]))
-                + self.__proxy[5]
-            )
+            packet = bytearray()
+            packet.append(0x01)
+            packet.append(len(self.__proxy[4]))
+            packet.extend(self.__proxy[4])
+            packet.append(len(self.__proxy[5]))
+            packet.extend(self.__proxy[5])
+            self.sendall(packet)
             authstat = self.__recvall(2)
             if authstat[0:1] != chr(0x01).encode():
                 # Bad response
