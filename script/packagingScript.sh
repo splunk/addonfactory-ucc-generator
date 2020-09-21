282389e055fd50563dac9f69d279a02434d0ed62
Raj Karkar <69803383+rajkarkar-crest@users.noreply.github.com>
2020-09-21 22:42:19 +0530

    Update packagingScript.sh

--- script/packagingScript.sh
+++ script/packagingScript.sh
@@ -42,6 +42,5 @@ sudo apt-get -qq install python-pip > /dev/null
 sudo apt-get -qq install -y libxml2-dev libxslt-dev lib32z1-dev python-lxml > /dev/null
 pip2 install "virtualenv<17.0.0,>=16.7.9" --upgrade > /dev/null
 
-pip3 install splunk_add_on_ucc_framework-4.0.5a5.post2.dev0+e93faced.tar.gz
-echo $VERSION_SPLUNK
-ucc-gen --ta-version $VERSION_SPLUNK
+pip3 install splunk_add_on_ucc_framework-0.1.0.tar.gz
+ucc-gen
