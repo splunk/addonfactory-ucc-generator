57a81ec9e00c809babd248c06280bae5b2ba1e1a
harshpatel <harshpatel@splunk.com>
2021-03-02 21:24:00 +0530

    ADDON-34283: ucc_ui_lib: updated webpack config according to guideline mentioned in react doc for code-splitting

--- splunk_add_on_ucc_framework/ucc_ui_lib/webpack.config.js
+++ splunk_add_on_ucc_framework/ucc_ui_lib/webpack.config.js
@@ -4,10 +4,12 @@ const webpackMerge = require('webpack-merge');
 const baseConfig = require('@splunk/webpack-configs/base.config').default;
 
 module.exports = webpackMerge(baseConfig, {
-    entry: path.join(__dirname, 'src/main/webapp/pages/entry_page'),
+    entry: {
+        entry_page: path.join(__dirname, 'src/main/webapp/pages/entry_page')
+    },
     output: {
         path: path.join(__dirname, 'stage/appserver/static/js/build'),
-        filename: 'entry_page.js',
+        filename: '[name].js',
     },
     plugins: [
         new CopyWebpackPlugin([
