20df10dc389aa96505009721aa3c7fe7c840f19c
Dishank <dkhatri@splunk.com>
2021-03-26 15:34:57 +0530

    ADDON-35152: Updated checkbox component to handle null values

--- splunk_add_on_ucc_framework/ucc_ui_lib/src/main/webapp/components/CheckBoxComponent.jsx
+++ splunk_add_on_ucc_framework/ucc_ui_lib/src/main/webapp/components/CheckBoxComponent.jsx
@@ -5,10 +5,10 @@ import { isFalse } from '../util/util';
 
 class CheckBoxComponent extends Component {
     handleChange = () => {
-        if (isFalse(this.props.value)) {
-            this.props.handleChange(this.props.field, 1);
-        } else {
+        if (this.props.value && !isFalse(this.props.value)) {
             this.props.handleChange(this.props.field, 0);
+        } else {
+            this.props.handleChange(this.props.field, 1);
         }
     };
 
@@ -19,7 +19,7 @@ class CheckBoxComponent extends Component {
                 value={this.props.field}
                 onClick={this.handleChange}
                 disabled={this.props.disabled}
-                selected={!isFalse(this.props.value)}
+                selected={!(this.props.value ? isFalse(this.props.value) : true)}
                 appearance="checkbox"
             />
         );
