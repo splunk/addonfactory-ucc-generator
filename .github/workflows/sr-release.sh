f05aaa86550014f6e956bd513f1da65db8fe9e67
rfaircloth-splunk <rfaircloth@splunk.com>
2021-06-08 11:24:36 -0400

    fix: fix script

--- .github/workflows/sr-release.sh
+++ .github/workflows/sr-release.sh
@@ -2,6 +2,6 @@
 
 set -eE
 set -v
-source $HOME/.poetry/env ;poetry publish 
-echo pypy user=$${PYPI_USERNAME}
+source $HOME/.poetry/env
+echo pypy user=${PYPI_USERNAME}
 poetry publish -n -u ${PYPI_USERNAME} -p ${PYPI_TOKEN}
