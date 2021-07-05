7448eb8d84b27cea225b01b72dffc7a8d6922321
Harsh Patel <harshpatel@splunk.com>
2021-07-05 14:15:25 +0000

    fix: skip `null` value fields from displaying in table expansion row

--- ui/src/main/webapp/components/table/TableExpansionRow.jsx
+++ ui/src/main/webapp/components/table/TableExpansionRow.jsx
@@ -18,7 +18,7 @@ function getExpansionRowData(row, moreInfo) {
         moreInfo.forEach((val) => {
             const label = _(val.label);
             // remove extra rows which are empty in moreInfo
-            if (val.field in row && row[val.field] !== '') {
+            if (val.field in row && row[val.field] !== null && row[val.field] !== '') {
                 DefinitionLists.push(<DL.Term key={val.field}>{label}</DL.Term>);
                 DefinitionLists.push(
                     <DL.Description key={`${val.field}_decr`}>
