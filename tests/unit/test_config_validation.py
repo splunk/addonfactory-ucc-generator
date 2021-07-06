70eb38ee8ed74722d911be19700f92a621428e16
Artem Rys <arys@splunk.com>
2021-07-06 12:25:07 +0200

    feat: support app.manifest file with comments
    Lines that start with "#" are supported. Some of the app.manifest files have comments.
    
    Appinspect tool does not fail if app.manifest contains comments. Exact lines of code from appinspect: https://github.com/splunk/appinspect/blob/fabd1387cf41e087edd86fb7f28e2f9e69a1467c/splunk_appinspect/checks/check_support_and_installation_standards.py#L169-L180

--- tests/unit/test_config_validation.py
+++ tests/unit/test_config_validation.py
@@ -14,7 +14,6 @@
 # limitations under the License.
 #
 import json
-import os.path
 import unittest
 
 import jsonschema
@@ -22,21 +21,14 @@ import tests.unit.helpers as helpers
 from splunk_add_on_ucc_framework import validate_config_against_schema
 
 
-def _get_config(config_name: str) -> dict:
-    config_path = os.path.join(
-        os.path.dirname(os.path.realpath(__file__)), "testdata", config_name
-    )
-    with open(config_path, "r") as f_config:
-        valid_config_raw = f_config.read()
-        return json.loads(valid_config_raw)
-
-
 class ConfigValidationTest(unittest.TestCase):
     def test_config_validation_when_valid_config_then_no_exception(self):
-        config = helpers.get_config("valid_config.json")
+        config = helpers.get_testdata_file("valid_config.json")
+        config = json.loads(config)
         validate_config_against_schema(config)
 
     def test_config_validation_when_invalid_config_then_exception(self):
-        config = helpers.get_config("invalid_config_no_configuration_tabs.json")
+        config = helpers.get_testdata_file("invalid_config_no_configuration_tabs.json")
+        config = json.loads(config)
         with self.assertRaises(jsonschema.ValidationError):
             validate_config_against_schema(config)
