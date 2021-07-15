d5adc4a69e7823b9272beb1df3484351a9e35f0f
Artem Rys <arys@splunk.com>
2021-07-15 17:29:43 +0200

    test: add tests for generating html files for modular inputs

---
+++ tests/unit/test_alert_actions_html_gen.py
@@ -0,0 +1,121 @@
+#
+# Copyright 2021 Splunk Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+import unittest
+from unittest import mock
+
+from splunk_add_on_ucc_framework.modular_alert_builder.build_core import (
+    alert_actions_html_gen,
+)
+from tests.unit.helpers import get_testdata_file
+
+
+class AlertActionsHtmlGenTest(unittest.TestCase):
+    def test_generate_alert_action(self):
+        generated = alert_actions_html_gen.generate_alert_actions_html_files(
+            input_setting={
+                "product_id": "Splunk_TA_UCCExample",
+                "short_name": "splunk_ta_uccexample",
+                "modular_alerts": [
+                    {
+                        "label": "Test Alert",
+                        "description": "Description for test Alert Action",
+                        "short_name": "test_alert",
+                        "active_response": {
+                            "task": ["Create", "Update"],
+                            "subject": ["endpoint"],
+                            "category": [
+                                "Information Conveyance",
+                                "Information Portrayal",
+                            ],
+                            "technology": [
+                                {
+                                    "version": ["1.0.0"],
+                                    "product": "Test Incident Update",
+                                    "vendor": "Splunk",
+                                }
+                            ],
+                            "sourcetype": "test:incident",
+                            "supports_adhoc": True,
+                            "drilldown_uri": 'search?q=search%20index%3D"_internal"&earliest=0&latest=',
+                        },
+                        "parameters": [
+                            {
+                                "label": "Name",
+                                "required": True,
+                                "format_type": "text",
+                                "name": "name",
+                                "default_value": "xyz",
+                                "help_string": "Please enter your name",
+                            },
+                            {
+                                "label": "All Incidents",
+                                "required": False,
+                                "format_type": "checkbox",
+                                "name": "all_incidents",
+                                "default_value": 0,
+                                "help_string": "Tick if you want to update all incidents/problems",
+                            },
+                            {
+                                "label": "Table List",
+                                "required": False,
+                                "format_type": "dropdownlist",
+                                "name": "table_list",
+                                "help_string": "Please select the table",
+                                "default_value": "problem",
+                                "possible_values": {
+                                    "incident": "Incident",
+                                    "problem": "Problem",
+                                },
+                            },
+                            {
+                                "label": "Action:",
+                                "required": True,
+                                "format_type": "radio",
+                                "name": "action",
+                                "help_string": "Select the action you want to perform",
+                                "default_value": "two",
+                                "possible_values": {
+                                    "update": "Update",
+                                    "delete": "Delete",
+                                },
+                            },
+                            {
+                                "label": "Select Account",
+                                "required": True,
+                                "format_type": "dropdownlist_splunk_search",
+                                "name": "account",
+                                "help_string": "Select the account from the dropdown",
+                                "ctrl_props": {
+                                    "value-field": "title",
+                                    "label-field": "title",
+                                    "search": "| rest /servicesNS/nobody/TA-SNOW/admin/TA_SNOW_account | dedup title",
+                                },
+                            },
+                        ],
+                        "alert_props": {
+                            "python.version": "python3",
+                            "is_custom": 1,
+                            "payload_format": "json",
+                        },
+                    }
+                ],
+            },
+            logger=mock.MagicMock(),
+            package_path=os.path.dirname(__file__),
+        )
+        expected_alert_html = get_testdata_file("alert.html.generated")
+        self.assertEqual(expected_alert_html, generated["test_alert"])
