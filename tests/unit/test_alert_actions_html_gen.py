c7078ef4f7b50113ad93a410b4cf4f7e43b2a4d1
Artem Rys <arys@splunk.com>
2021-07-27 09:15:24 +0200

    test: use temporary directory

--- tests/unit/test_alert_actions_html_gen.py
+++ tests/unit/test_alert_actions_html_gen.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import os
+import tempfile
 import unittest
 from unittest import mock
 
@@ -25,97 +25,98 @@ from tests.unit.helpers import get_testdata_file
 
 class AlertActionsHtmlGenTest(unittest.TestCase):
     def test_generate_alert_action(self):
-        generated = alert_actions_html_gen.generate_alert_actions_html_files(
-            input_setting={
-                "product_id": "Splunk_TA_UCCExample",
-                "short_name": "splunk_ta_uccexample",
-                "modular_alerts": [
-                    {
-                        "label": "Test Alert",
-                        "description": "Description for test Alert Action",
-                        "short_name": "test_alert",
-                        "active_response": {
-                            "task": ["Create", "Update"],
-                            "subject": ["endpoint"],
-                            "category": [
-                                "Information Conveyance",
-                                "Information Portrayal",
-                            ],
-                            "technology": [
+        with tempfile.TemporaryDirectory() as temp_dir:
+            generated = alert_actions_html_gen.generate_alert_actions_html_files(
+                input_setting={
+                    "product_id": "Splunk_TA_UCCExample",
+                    "short_name": "splunk_ta_uccexample",
+                    "modular_alerts": [
+                        {
+                            "label": "Test Alert",
+                            "description": "Description for test Alert Action",
+                            "short_name": "test_alert",
+                            "active_response": {
+                                "task": ["Create", "Update"],
+                                "subject": ["endpoint"],
+                                "category": [
+                                    "Information Conveyance",
+                                    "Information Portrayal",
+                                ],
+                                "technology": [
+                                    {
+                                        "version": ["1.0.0"],
+                                        "product": "Test Incident Update",
+                                        "vendor": "Splunk",
+                                    }
+                                ],
+                                "sourcetype": "test:incident",
+                                "supports_adhoc": True,
+                                "drilldown_uri": 'search?q=search%20index%3D"_internal"&earliest=0&latest=',
+                            },
+                            "parameters": [
                                 {
-                                    "version": ["1.0.0"],
-                                    "product": "Test Incident Update",
-                                    "vendor": "Splunk",
-                                }
+                                    "label": "Name",
+                                    "required": True,
+                                    "format_type": "text",
+                                    "name": "name",
+                                    "default_value": "xyz",
+                                    "help_string": "Please enter your name",
+                                },
+                                {
+                                    "label": "All Incidents",
+                                    "required": False,
+                                    "format_type": "checkbox",
+                                    "name": "all_incidents",
+                                    "default_value": 0,
+                                    "help_string": "Tick if you want to update all incidents/problems",
+                                },
+                                {
+                                    "label": "Table List",
+                                    "required": False,
+                                    "format_type": "dropdownlist",
+                                    "name": "table_list",
+                                    "help_string": "Please select the table",
+                                    "default_value": "problem",
+                                    "possible_values": {
+                                        "incident": "Incident",
+                                        "problem": "Problem",
+                                    },
+                                },
+                                {
+                                    "label": "Action:",
+                                    "required": True,
+                                    "format_type": "radio",
+                                    "name": "action",
+                                    "help_string": "Select the action you want to perform",
+                                    "default_value": "two",
+                                    "possible_values": {
+                                        "update": "Update",
+                                        "delete": "Delete",
+                                    },
+                                },
+                                {
+                                    "label": "Select Account",
+                                    "required": True,
+                                    "format_type": "dropdownlist_splunk_search",
+                                    "name": "account",
+                                    "help_string": "Select the account from the dropdown",
+                                    "ctrl_props": {
+                                        "value-field": "title",
+                                        "label-field": "title",
+                                        "search": "| rest /servicesNS/nobody/TA-SNOW/admin/TA_SNOW_account | dedup title",
+                                    },
+                                },
                             ],
-                            "sourcetype": "test:incident",
-                            "supports_adhoc": True,
-                            "drilldown_uri": 'search?q=search%20index%3D"_internal"&earliest=0&latest=',
-                        },
-                        "parameters": [
-                            {
-                                "label": "Name",
-                                "required": True,
-                                "format_type": "text",
-                                "name": "name",
-                                "default_value": "xyz",
-                                "help_string": "Please enter your name",
+                            "alert_props": {
+                                "python.version": "python3",
+                                "is_custom": 1,
+                                "payload_format": "json",
                             },
-                            {
-                                "label": "All Incidents",
-                                "required": False,
-                                "format_type": "checkbox",
-                                "name": "all_incidents",
-                                "default_value": 0,
-                                "help_string": "Tick if you want to update all incidents/problems",
-                            },
-                            {
-                                "label": "Table List",
-                                "required": False,
-                                "format_type": "dropdownlist",
-                                "name": "table_list",
-                                "help_string": "Please select the table",
-                                "default_value": "problem",
-                                "possible_values": {
-                                    "incident": "Incident",
-                                    "problem": "Problem",
-                                },
-                            },
-                            {
-                                "label": "Action:",
-                                "required": True,
-                                "format_type": "radio",
-                                "name": "action",
-                                "help_string": "Select the action you want to perform",
-                                "default_value": "two",
-                                "possible_values": {
-                                    "update": "Update",
-                                    "delete": "Delete",
-                                },
-                            },
-                            {
-                                "label": "Select Account",
-                                "required": True,
-                                "format_type": "dropdownlist_splunk_search",
-                                "name": "account",
-                                "help_string": "Select the account from the dropdown",
-                                "ctrl_props": {
-                                    "value-field": "title",
-                                    "label-field": "title",
-                                    "search": "| rest /servicesNS/nobody/TA-SNOW/admin/TA_SNOW_account | dedup title",
-                                },
-                            },
-                        ],
-                        "alert_props": {
-                            "python.version": "python3",
-                            "is_custom": 1,
-                            "payload_format": "json",
-                        },
-                    }
-                ],
-            },
-            logger=mock.MagicMock(),
-            package_path=os.path.dirname(__file__),
-        )
-        expected_alert_html = get_testdata_file("alert.html.generated")
-        self.assertEqual(expected_alert_html, generated["test_alert"])
+                        }
+                    ],
+                },
+                logger=mock.MagicMock(),
+                package_path=temp_dir,
+            )
+            expected_alert_html = get_testdata_file("alert.html.generated")
+            self.assertEqual(expected_alert_html, generated["test_alert"])
