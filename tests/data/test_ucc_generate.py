a5c3c3e3ff89dcf614c78fb93d3488835f1f6a55
Artem Rys <arys@splunk.com>
2021-08-07 15:07:41 +0200

    test: add test for package with only configuration

--- tests/data/test_ucc_generate.py
+++ tests/data/test_ucc_generate.py
@@ -15,7 +15,7 @@ class UccGenerateTest(unittest.TestCase):
         )
         ucc.generate(source=package_folder)
 
-    def test_ucc_generate_with_custom_output_folder(self):
+    def test_ucc_generate_with_inputs_configuration_alerts(self):
         with tempfile.TemporaryDirectory() as temp_dir:
             package_folder = path.join(
                 path.dirname(path.realpath(__file__)),
@@ -61,6 +61,53 @@ class UccGenerateTest(unittest.TestCase):
                 ("bin", "test_alert.py"),
                 ("README", "alert_actions.conf.spec"),
                 ("README", "inputs.conf.spec"),
+                ("README", "splunk_ta_uccexample_account.conf.spec"),
+                ("README", "splunk_ta_uccexample_settings.conf.spec"),
+            ]
+            for f in files_to_be_equal:
+                expected_file_path = path.join(expected_folder, *f)
+                actual_file_path = path.join(actual_folder, *f)
+                self.assertTrue(
+                    assert_identical_files(
+                        expected_file_path,
+                        actual_file_path,
+                    ),
+                    msg=f"Expected file {expected_file_path} is different from {actual_file_path}",
+                )
+
+    def test_ucc_generate_with_configuration(self):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            package_folder = path.join(
+                path.dirname(path.realpath(__file__)),
+                "package_global_config_configuration",
+                "package",
+            )
+            ucc.generate(source=package_folder, outputdir=temp_dir)
+
+            expected_folder = path.join(
+                path.dirname(__file__),
+                "..",
+                "expected_output_global_config_configuration",
+                "Splunk_TA_UCCExample",
+            )
+            actual_folder = path.join(temp_dir, "Splunk_TA_UCCExample")
+
+            # app.manifest and appserver/static/js/build/globalConfig.json
+            # should be included too, but they may introduce flaky tests as
+            # their content depends on the git commit.
+            # Expected add-on package folder does not have "lib" in it.
+            files_to_be_equal = [
+                ("README.txt",),
+                ("default", "restmap.conf"),
+                ("default", "splunk_ta_uccexample_settings.conf"),
+                ("default", "web.conf"),
+                ("default", "data", "ui", "nav", "default.xml"),
+                ("default", "data", "ui", "views", "configuration.xml"),
+                ("default", "data", "ui", "views", "splunk_ta_uccexample_redirect.xml"),
+                ("bin", "import_declare_test.py"),
+                ("bin", "splunk_ta_uccexample_rh_account.py"),
+                ("bin", "splunk_ta_uccexample_rh_oauth.py"),
+                ("bin", "splunk_ta_uccexample_rh_settings.py"),
                 ("README", "splunk_ta_uccexample_account.conf.spec"),
                 ("README", "splunk_ta_uccexample_settings.conf.spec"),
             ]
---
+++ tests/expected_output_global_config_configuration/Splunk_TA_UCCExample/bin/splunk_ta_uccexample_rh_account.py
@@ -0,0 +1,146 @@
+
+import import_declare_test
+
+from splunktaucclib.rest_handler.endpoint import (
+    field,
+    validator,
+    RestModel,
+    SingleModel,
+)
+from splunktaucclib.rest_handler import admin_external, util
+from splunktaucclib.rest_handler.admin_external import AdminExternalHandler
+import logging
+
+util.remove_http_proxy_env_vars()
+
+
+fields = [
+    field.RestField(
+        'custom_endpoint',
+        required=True,
+        encrypted=False,
+        default='login.example.com',
+        validator=None
+    ), 
+    field.RestField(
+        'endpoint',
+        required=False,
+        encrypted=False,
+        default=None,
+        validator=None
+    ), 
+    field.RestField(
+        'account_checkbox',
+        required=False,
+        encrypted=False,
+        default=None,
+        validator=None
+    ), 
+    field.RestField(
+        'account_radio',
+        required=True,
+        encrypted=False,
+        default='yes',
+        validator=None
+    ), 
+    field.RestField(
+        'account_multiple_select',
+        required=True,
+        encrypted=False,
+        default=None,
+        validator=None
+    ), 
+    field.RestField(
+        'example_help_link',
+        required=False,
+        encrypted=False,
+        default=None,
+        validator=None
+    ), 
+    field.RestField(
+        'username',
+        required=False,
+        encrypted=False,
+        default=None,
+        validator=None
+    ), 
+    field.RestField(
+        'password',
+        required=False,
+        encrypted=True,
+        default=None,
+        validator=None
+    ), 
+    field.RestField(
+        'token',
+        required=False,
+        encrypted=True,
+        default=None,
+        validator=None
+    ), 
+    field.RestField(
+        'client_id',
+        required=False,
+        encrypted=False,
+        default=None,
+        validator=None
+    ), 
+    field.RestField(
+        'client_secret',
+        required=False,
+        encrypted=True,
+        default=None,
+        validator=None
+    ), 
+    field.RestField(
+        'redirect_url',
+        required=False,
+        encrypted=False,
+        default=None,
+        validator=None
+    ), 
+    field.RestField(
+        'access_token',
+        required=False,
+        encrypted=True,
+        default=None,
+        validator=None
+    ), 
+    field.RestField(
+        'refresh_token',
+        required=False,
+        encrypted=True,
+        default=None,
+        validator=None
+    ), 
+    field.RestField(
+        'instance_url',
+        required=False,
+        encrypted=False,
+        default=None,
+        validator=None
+    ), 
+    field.RestField(
+        'auth_type',
+        required=False,
+        encrypted=False,
+        default=None,
+        validator=None
+    )
+]
+model = RestModel(fields, name=None)
+
+
+endpoint = SingleModel(
+    'splunk_ta_uccexample_account',
+    model,
+    config_name='account'
+)
+
+
+if __name__ == '__main__':
+    logging.getLogger().addHandler(logging.NullHandler())
+    admin_external.handle(
+        endpoint,
+        handler=AdminExternalHandler,
+    )
