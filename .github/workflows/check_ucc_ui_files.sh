7c4694dc89f1ea255444cecd86da2413e7a5f4c3
Artem Rys <arys@splunk.com>
2021-08-14 20:06:49 +0200

    ci: fix issues after merges

--- .github/workflows/check_ucc_ui_files.sh
+++ .github/workflows/check_ucc_ui_files.sh
@@ -3,9 +3,16 @@
 set -eE
 set -v
 BUILT_FOLDER=/tmp/ucc_built
+# Creates folder for output folder.
 mkdir -p $BUILT_FOLDER
-tar -zxf dist/splunk_add_on_ucc_framework-0.0.0.tar.gz -C $BUILT_FOLDER
-UCC_FOLDER=$BUILT_FOLDER/splunk_add_on_ucc_framework-0.0.0/splunk_add_on_ucc_framework
+# Gets the name of the archive after running `poetry build`.
+ARCHIVE_NAME=$(find dist -name '*.tar.gz' -exec basename {} \;)
+# Gets folder name from archive name. Removes .tar.gz ending from the archive name.
+FOLDER_NAME=$(echo "$ARCHIVE_NAME" | sed -E "s/.tar.gz//")
+# Extracts archive to $BUILT_FOLDER.
+tar -zxf dist/"$ARCHIVE_NAME" -C $BUILT_FOLDER
+# Builds a path to UCC to check UI files and folders.
+UCC_FOLDER=$BUILT_FOLDER/$FOLDER_NAME/splunk_add_on_ucc_framework
 if [[ ! -d $UCC_FOLDER/package/ ]]
 then
   echo "package/ folder does not exist"
