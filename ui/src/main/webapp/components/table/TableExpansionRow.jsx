fe172524fe103a1b06eff0e9120eb770fad83cee
harshpatel-crest <70878308+harshpatel-crest@users.noreply.github.com>
2022-01-31 15:26:03 +0530

    fix: resolve missed `type` param for custom input row feature (#84)

--- ui/src/main/webapp/components/table/TableExpansionRow.jsx
+++ ui/src/main/webapp/components/table/TableExpansionRow.jsx
@@ -45,6 +45,7 @@ export function getExpansionRow(colSpan, row, moreInfo) {
                             serviceName: row.serviceName,
                             row,
                             fileName: customRow.src,
+                            type: customRow.type,
                         })}
                     </>
                 ) : (
