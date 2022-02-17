219980ddcfbf6db14ce137ee4d53c7fc1dc85ddb
Artem Rys <rysartem@gmail.com>
2022-02-17 10:58:21 +0100

    ci: fix release (#432)

--- .github/workflows/sr-prepare.sh
+++ .github/workflows/sr-prepare.sh
@@ -2,6 +2,4 @@
 
 set -eE
 set -v
-# shellcheck disable=SC1091,SC2086
-source $HOME/.poetry/env
 poetry build
--- .github/workflows/sr-release.sh
+++ .github/workflows/sr-release.sh
@@ -2,6 +2,5 @@
 
 set -eE
 set -v
-source $HOME/.poetry/env
 echo pypy user=${PYPI_USERNAME}
 poetry publish -n -u ${PYPI_USERNAME} -p ${PYPI_TOKEN}
