1efd7b2536e7d45a55d30e3834b65cc7e4f76ad0
Artem Rys <rysartem@gmail.com>
2022-09-07 18:03:02 +0200

    style: pre-commit run

--- splunk_add_on_ucc_framework/commands/build.py
+++ splunk_add_on_ucc_framework/commands/build.py
@@ -43,7 +43,9 @@ logger = logging.getLogger("ucc_gen")
 
 PARENT_DIR = ".."
 internal_root_dir = os.path.dirname(os.path.dirname(__file__))
-j2_env = Environment(loader=FileSystemLoader(os.path.join(internal_root_dir, "templates")))
+j2_env = Environment(
+    loader=FileSystemLoader(os.path.join(internal_root_dir, "templates"))
+)
 
 
 def _update_ta_version(config, ta_version):
