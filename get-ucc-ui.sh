20d2a5c50704d65669028967980712ea6a7939f2
Artem Rys <rysartem@gmail.com>
2022-11-28 10:15:05 +0100

    fix: update UCC UI to 1.14.2 (#573)

--- get-ucc-ui.sh
+++ get-ucc-ui.sh
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-wget https://github.com/splunk/addonfactory-ucc-base-ui/releases/download/v1.14.1/splunk-ucc-ui.tgz
+wget https://github.com/splunk/addonfactory-ucc-base-ui/releases/download/v1.14.2/splunk-ucc-ui.tgz
