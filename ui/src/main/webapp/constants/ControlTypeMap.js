69a62b542b9f05722bc395d3c4608e4dbc8ef18a
Tushar Balar <62089106+tbalar-splunk@users.noreply.github.com>
2022-12-01 18:31:13 +0530

    feat(CODE): ADDON-57044 Added new support for Textarea field (#180)

--- ui/src/main/webapp/constants/ControlTypeMap.js
+++ ui/src/main/webapp/constants/ControlTypeMap.js
@@ -1,5 +1,6 @@
 import HelpLinkComponent from '../components/HelpLinkComponent';
 import TextComponent from '../components/TextComponent';
+import TextAreaComponent from '../components/TextAreaComponent';
 import SingleInputComponent from '../components/SingleInputComponent';
 import MultiInputComponent from '../components/MultiInputComponent';
 import CheckBoxComponent from '../components/CheckBoxComponent';
@@ -9,6 +10,7 @@ import CustomControl from '../components/CustomControl';
 
 export default {
     text: TextComponent,
+    textarea: TextAreaComponent,
     singleSelect: SingleInputComponent,
     helpLink: HelpLinkComponent,
     multipleSelect: MultiInputComponent,
