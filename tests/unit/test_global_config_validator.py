2a967e5096ca566bbabab6edd778e4f6c1c2d6f8
mbruzda <mbruzda@splunk.com>
2023-02-07 15:16:28 +0100

    added tests for inputs

--- tests/unit/test_global_config_validator.py
+++ tests/unit/test_global_config_validator.py
@@ -217,6 +217,26 @@ def test_config_validation_when_valid(filename):
             pytest.raises(GlobalConfigValidatorException),
             "Duplicates found for tabs names or titles",
         ),
+        (
+            "invalid_config_inputs_services_duplicates.json",
+            pytest.raises(GlobalConfigValidatorException),
+            "Duplicates found for inputs (services) names or titles"
+        ),
+        (
+            "invalid_config_inputs_entity_duplicates.json",
+            pytest.raises(GlobalConfigValidatorException),
+            "Duplicates found for entity field or label"
+        ),
+        (
+            "invalid_config_inputs_children_duplicates.json",
+            pytest.raises(GlobalConfigValidatorException),
+            "Duplicates found for autoCompleteFields children in entity Single Select"
+        ),
+        (
+            "invalid_config_inputs_autoCompleteFields_duplicates.json",
+            pytest.raises(GlobalConfigValidatorException),
+            "Duplicates found for autoCompleteFields: Single Select"
+        )
     ],
 )
 def test_config_validation_when_error(filename, expectation, exception_message):
