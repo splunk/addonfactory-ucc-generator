d7a5fd83d344bbb9e2420f108e0c3df743d14d6a
Artem Rys <rysartem@gmail.com>
2023-04-04 16:12:53 +0200

    test: fix pipeline failures

--- tests/smoke/test_ucc_import_from_aob.py
+++ tests/smoke/test_ucc_import_from_aob.py
@@ -12,7 +12,11 @@ def test_ucc_import_from_aob():
     Splunkbase directly and moved to this folder, so it will be easier to
     execute tests.
     """
-    with tarfile.open("dynatrace-add-on-for-splunk_122.tgz", "r:gz") as tar:
+    aob_addon_path = os.path.join(
+        os.path.dirname(__file__),
+        "dynatrace-add-on-for-splunk_122.tgz",
+    )
+    with tarfile.open(aob_addon_path, "r:gz") as tar:
         tar.extractall()
     import_from_aob.import_from_aob("Splunk_TA_Dynatrace")
     build.generate(source=os.path.join("Splunk_TA_Dynatrace_ucc", "package"))
