d1a5af12bcef08d66ccd1d85d8e166a2a1fd3222
Tushar Balar <62089106+tbalar-splunk@users.noreply.github.com>
2023-06-22 18:20:07 +0530

    fix: ADDON-62948 Added support for subTitle field in service (#403)
    * feat: ADDON-refactor8 Added support for subTitle field in service
    
    * refactor: ADDON-62948 Added support for subTitle field in service

--- ui/src/main/webapp/components/MenuInput.jsx
+++ ui/src/main/webapp/components/MenuInput.jsx
@@ -117,20 +117,20 @@ function MenuInput({ handleRequestOpen }) {
                             hasSubmenu: false,
                             title: services.find((service) => service.name === serviceName).title,
                             subTitle: services.find((service) => service.name === serviceName)
-                                .subTitle,
+                                ?.subTitle,
                         });
                     });
                     servicesGroup.main_panel.push({
                         name: group.groupName,
                         title: group.groupTitle,
-                        subTitle: services.find((service) => service.name === group.groupName)
-                            .subTitle,
                         hasSubmenu: true,
                     });
                 } else {
                     servicesGroup.main_panel.push({
                         name: group.groupName,
                         title: group.groupTitle,
+                        subTitle: services.find((service) => service.name === group.groupName)
+                            ?.subTitle,
                         hasSubmenu: false,
                     });
                 }
