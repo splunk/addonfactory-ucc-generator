86f6fcbcf9be64a56080e9d2282a3e771a119ce2
Daniel Federschmidt <dfederschmidt@splunk.com>
2023-09-29 15:09:23 +0200

    test: create test for python version identification

--- tests/unit/commands/test_build.py
+++ tests/unit/commands/test_build.py
@@ -1,8 +1,10 @@
 import os
+from unittest.mock import MagicMock, patch
 
 import pytest
 
-from splunk_add_on_ucc_framework.commands.build import _get_build_output_path
+from splunk_add_on_ucc_framework.commands.build import _get_build_output_path, _get_python_version_from_executable
+from splunk_add_on_ucc_framework.exceptions import CouldNotIdentifyPythonVersionException
 
 CURRENT_PATH = os.getcwd()
 
@@ -18,3 +20,28 @@ CURRENT_PATH = os.getcwd()
 )
 def test_get_build_output_path(output_directory, expected_output_directory):
     assert expected_output_directory == _get_build_output_path(output_directory)
+
+@patch("splunk_add_on_ucc_framework.commands.build.subprocess.run")
+def test_get_python_version_from_executable(mock_run):
+    target_python_version = "Python 3.8.17"
+
+    mock_stdout = MagicMock()
+    mock_stdout.configure_mock(
+        **{
+            "stdout.decode.return_value": target_python_version
+        }
+    )
+
+    mock_run.return_value = mock_stdout
+
+    python_version = _get_python_version_from_executable("python3")
+
+    assert python_version == target_python_version
+
+def test_get_python_version_from_executable_nonexisting_command():
+    target_python_version = "acommandthatdoesnotexistda39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+    with pytest.raises(CouldNotIdentifyPythonVersionException):
+        _get_python_version_from_executable(target_python_version)
+
+
