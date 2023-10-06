36edeac339f965ab79c335a8e14672b576af3c7a
Artem Rys <rysartem@gmail.com>
2023-10-06 21:07:10 +0200

    ci: rollback versions to trigger the release

--- splunk_add_on_ucc_framework/__init__.py
+++ splunk_add_on_ucc_framework/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-__version__ = "5.31.1-beta.1"
+__version__ = "5.31.0"
 
 import logging
 
