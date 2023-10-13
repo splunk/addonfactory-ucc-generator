d8b837c72507f431f0ba5985d44ecd9d25d07d53
Viktor Tsvetkov <142901247+vtsvetkov-splunk@users.noreply.github.com>
2023-10-13 13:00:51 +0200

    replace with Number component

--- ui/src/main/webapp/components/CheckboxGroup/CheckboxRow.tsx
+++ ui/src/main/webapp/components/CheckboxGroup/CheckboxRow.tsx
@@ -1,16 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import ColumnLayout from '@splunk/react-ui/ColumnLayout';
-import Text from '@splunk/react-ui/Text';
-import { TextChangeHandler } from '@splunk/react-ui/types/src/Text/Text';
+import NumberComponent, { NumberChangeHandler } from '@splunk/react-ui/Number';
 import styled from 'styled-components';
 import Switch from '@splunk/react-ui/Switch';
 import { StyledColumnLayout } from './StyledComponent';
 
 const StyledSwitch = styled(Switch)`
     padding-left: 3px;
-`;
-const StyledText = styled(Text)`
-    padding-right: 3px;
 `;
 
 interface CheckboxRowProps {
@@ -31,10 +27,8 @@ function CheckboxRow(props: CheckboxRowProps) {
         setIsTextDisabled(!checkbox || disabled);
     }, [checkbox, disabled]);
 
-    const handleChangeTextBox: TextChangeHandler = (event) => {
-        if (event?.target && 'value' in event.target) {
-            handleChange({ field, inputValue: Number(event.target.value), checkbox });
-        }
+    const handleChangeInput: NumberChangeHandler = (event: unknown, { value }) => {
+        handleChange({ field, inputValue: value, checkbox });
     };
 
     const handleChangeCheckbox = (event: unknown, data: { selected: boolean; value?: unknown }) => {
@@ -45,7 +39,7 @@ function CheckboxRow(props: CheckboxRowProps) {
     return (
         <StyledColumnLayout>
             <ColumnLayout.Row>
-                <ColumnLayout.Column span={5}>
+                <ColumnLayout.Column span={7}>
                     <StyledSwitch
                         key={field}
                         value={field}
@@ -57,14 +51,12 @@ function CheckboxRow(props: CheckboxRowProps) {
                         {label}
                     </StyledSwitch>
                 </ColumnLayout.Column>
-                <ColumnLayout.Column span={2}>
-                    <StyledText
+                <ColumnLayout.Column span={3}>
+                    <NumberComponent
                         inline
                         disabled={isTextDisabled}
-                        value={input?.toString()}
-                        onChange={handleChangeTextBox}
-                        type="number"
-                        required
+                        defaultValue={input}
+                        onChange={handleChangeInput}
                     />
                 </ColumnLayout.Column>
             </ColumnLayout.Row>
