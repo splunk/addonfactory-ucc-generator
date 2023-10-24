51cf317bf2a340ef5e475f341741ef94ded6cc59
Hubert Sękowski <hsekowski@splunk.com>
2023-10-24 12:31:55 +0200

    chore: smoke tests updated for updated init command

--- tests/testdata/expected_addons/expected_addon_after_init/demo_addon_for_splunk/package/bin/demo_input.py
+++ tests/testdata/expected_addons/expected_addon_after_init/demo_addon_for_splunk/package/bin/demo_input.py
@@ -7,35 +7,20 @@ import import_declare_test
 from solnlib import conf_manager, log
 from splunklib import modularinput as smi
 
-ADDON_NAME = "demo_addon_for_splunk"
+from demo_addon_for_splunk_utils import ADDON_NAME, Connect, set_logger_for_input
 
-
-def logger_for_input(input_name: str) -> logging.Logger:
-    return log.Logs().get_logger(f"{ADDON_NAME.lower()}_{input_name}")
-
-
-def get_account_api_key(session_key: str, account_name: str):
+def get_endpoint_uri(session_key: str, endpoint_name: str):
     cfm = conf_manager.ConfManager(
         session_key,
         ADDON_NAME,
-        realm=f"__REST_CREDENTIAL__#{ADDON_NAME}#configs/conf-demo_addon_for_splunk_account",
+        realm=f"__REST_CREDENTIAL__#{ADDON_NAME}#configs/conf-demo_addon_for_splunk_endpoint",
     )
-    account_conf_file = cfm.get_conf("demo_addon_for_splunk_account")
-    return account_conf_file.get(account_name).get("api_key")
+    endpoint_conf_file = cfm.get_conf("demo_addon_for_splunk_endpoint")
+    return endpoint_conf_file.get(endpoint_name).get("uri")
 
-
-def get_data_from_api(logger: logging.Logger, api_key: str):
-    logger.info("Getting data from an external API")
-    dummy_data = [
-        {
-            "line1": "hello",
-        },
-        {
-            "line2": "world",
-        },
-    ]
-    return dummy_data
-
+def get_data(logger: logging.Logger, uri: str):
+    logger.info(f"Getting data from an external URI {uri}")
+    return Connect(logger=logger).get(uri=uri)
 
 class Input(smi.Script):
     def __init__(self):
@@ -58,44 +43,34 @@ class Input(smi.Script):
         return
 
     def stream_events(self, inputs: smi.InputDefinition, event_writer: smi.EventWriter):
-        # inputs.inputs is a Python dictionary object like:
-        # {
-        #   "demo_input://<input_name>": {
-        #     "account": "<account_name>",
-        #     "disabled": "0",
-        #     "host": "$decideOnStartup",
-        #     "index": "<index_name>",
-        #     "interval": "<interval_value>",
-        #     "python.version": "python3",
-        #   },
-        # }
         for input_name, input_item in inputs.inputs.items():
             normalized_input_name = input_name.split("/")[-1]
-            logger = logger_for_input(normalized_input_name)
             try:
                 session_key = self._input_definition.metadata["session_key"]
-                log_level = conf_manager.get_log_level(
-                    logger=logger,
-                    session_key=session_key,
-                    app_name=ADDON_NAME,
-                    conf_name=f"{ADDON_NAME}_settings",
-                )
-                logger.setLevel(log_level)
+                logger = set_logger_for_input(session_key, normalized_input_name)
                 log.modular_input_start(logger, normalized_input_name)
-                api_key = get_account_api_key(session_key, input_item.get("account"))
-                data = get_data_from_api(logger, api_key)
-                sourcetype = "dummy-data"
-                for line in data:
+                uri = get_endpoint_uri(session_key, input_item.get("endpoint"))
+                response = get_data(logger, uri)
+                data=json.dumps(response.__dict__, ensure_ascii=False, default=str)
+                successul_or_not_successul = "successul" if response.ok else "not successul"
+                log_msg = f"""Request to {uri} was {successul_or_not_successul}
+Response details:
+{data}"""
+                if response.ok:
+                    logger.debug(log_msg)
                     event_writer.write_event(
                         smi.Event(
-                            data=json.dumps(line, ensure_ascii=False, default=str),
+                            data=data,
                             index=input_item.get("index"),
-                            sourcetype=sourcetype,
+                            sourcetype=input_item.get("sourcetype"),
+                            source=uri,
                         )
                     )
-                log.events_ingested(
-                    logger, normalized_input_name, sourcetype, len(data)
-                )
+                    logger.info(
+                        "Data was successfuly indexed"
+                    )
+                else:
+                    logger.error(log_msg)
                 log.modular_input_end(logger, normalized_input_name)
             except Exception as e:
                 logger.error(
