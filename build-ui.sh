7d338f705af6ec65344b35607d227f98417f5d50
Viktor Tsvetkov <142901247+vtsvetkov-splunk@users.noreply.github.com>
2023-10-24 12:00:11 +0200

    fix cp

--- build-ui.sh
+++ build-ui.sh
@@ -3,4 +3,4 @@
 cd ui && yarn run setup
 cd ..
 
-cp -n --recursive ui/dist/build/ splunk_add_on_ucc_framework/package/appserver/static/js/build/
+cp -n -R ui/dist/build/ splunk_add_on_ucc_framework/package/appserver/static/js/build/
