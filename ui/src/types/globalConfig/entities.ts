013ecdcb299a458343190b35323df8b9471ba06e
Viktor Tsvetkov <142901247+vtsvetkov-splunk@users.noreply.github.com>
2023-11-08 17:44:58 +0100

    wip

--- ui/src/types/globalConfig/entities.ts
+++ ui/src/types/globalConfig/entities.ts
@@ -13,7 +13,6 @@ const CommonEntityFields = z.object({
     type: z.string(),
     field: z.string(),
     label: z.string(),
-    options: z.object({ text: z.string(), link: z.string() }),
     help: z.string().optional(),
     tooltip: z.string().optional(),
 });
@@ -23,15 +22,12 @@ const CommonEditableEntityFields = CommonEntityFields.extend({
     encrypted: z.boolean().default(false),
 });
 
-const CommonEditableEntityOptions = z.object({});
-
-export const LinkEntity = CommonEntityFields.extend({
-    type: z.literal('helpLink'),
-    label: z.string().optional(),
-    options: z.object({ text: z.string(), link: z.string() }),
+const CommonEditableEntityOptions = z.object({
+    placeholder: z.string().optional(),
+    display: z.boolean().default(true),
+    disableonEdit: z.boolean().default(false),
+    enable: z.boolean().default(true),
 });
-
-export type LinkEntity = z.infer<typeof LinkEntity>;
 
 const AllValidators = z.array(
     z.union([
@@ -44,12 +40,57 @@ const AllValidators = z.array(
         DateValidator,
     ])
 );
-export const TextEntity = z.object({
+
+export const LinkEntity = CommonEntityFields.extend({
+    type: z.literal('helpLink'),
+    label: z.string().optional(),
+    options: z.object({ text: z.string(), link: z.string() }),
+});
+
+export type LinkEntity = z.infer<typeof LinkEntity>;
+
+export const TextEntity = CommonEditableEntityFields.extend({
     type: z.literal('text'),
-    field: z.string(),
+    validators: AllValidators.optional(),
+    defaultValue: z.union([z.string(), z.number()]).optional(),
+    options: CommonEditableEntityOptions.optional(),
+});
+
+export const TextAreaEntity = CommonEditableEntityFields.extend({
+    type: z.literal('textarea'),
+    validators: AllValidators.optional(),
+    defaultValue: z.string().optional(),
+    options: CommonEditableEntityOptions.extend({
+        rowsMin: z.number().optional(),
+        rowsMax: z.number().optional(),
+    }).optional(),
+});
+
+const autoCompleteBaseSchema = z.object({
     label: z.string(),
-    validators: AllValidators,
-    options: CommonEditableEntityOptions.extend({}),
-    help: z.string().optional(),
-    tooltip: z.string().optional(),
+});
+
+type AutoCompleteFields = z.infer<typeof autoCompleteBaseSchema> & {};
+
+const AutoCompleteFields = z.union([]);
+const ValueLabelPair = z.object({
+    value: z.string(),
+    label: z.string(),
+});
+
+export const SingleSelectEntity = CommonEditableEntityFields.extend({
+    type: z.literal('singleSelect'),
+    validators: AllValidators.optional(),
+    defaultValue: z.union([z.string(), z.number()]).optional(),
+    options: CommonEditableEntityOptions.extend({
+        createSearchChoice: z.boolean().optional(),
+        referenceName: z.string().optional(),
+        endpointUrl: z.string().optional(),
+        allowList: z.string().optional(),
+        denyList: z.string().optional(),
+        labelField: z.string().optional(),
+        autoCompleteFields: AutoCompleteFields.optional(),
+        dependencies: z.array(z.string()).optional(),
+        items: z.array(ValueLabelPair).optional(),
+    }),
 });
