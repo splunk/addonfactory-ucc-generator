32ca0a04f434521654fe0cd512f3f218d86d5fa8
mkolasinski-splunk <mkolasinski@splunk.com>
2023-11-21 09:44:13 +0100

    chore: cleanup imports

--- tests/ui/pages/configuration_page.py
+++ tests/ui/pages/configuration_page.py
@@ -1,27 +1,16 @@
 from pytest_splunk_addon_ui_smartx.pages.page import Page
 from pytest_splunk_addon_ui_smartx.components.base_component import Selector
-from pytest_splunk_addon_ui_smartx.components.base_component import BaseComponent
-from pytest_splunk_addon_ui_smartx.components.tabs import Tab
-from pytest_splunk_addon_ui_smartx.components.entity import Entity
-from pytest_splunk_addon_ui_smartx.components.controls.button import Button
-from pytest_splunk_addon_ui_smartx.components.controls.single_select import SingleSelect
-from pytest_splunk_addon_ui_smartx.components.controls.oauth_select import OAuthSelect
-from pytest_splunk_addon_ui_smartx.components.controls.multi_select import MultiSelect
-from pytest_splunk_addon_ui_smartx.components.controls.checkbox import Checkbox
-from pytest_splunk_addon_ui_smartx.components.controls.textbox import TextBox
-from pytest_splunk_addon_ui_smartx.components.controls.learn_more import LearnMore
-from pytest_splunk_addon_ui_smartx.components.controls.toggle import Toggle
 from pytest_splunk_addon_ui_smartx.components.controls.message import Message
-from pytest_splunk_addon_ui_smartx.components.conf_table import ConfigurationTable
-from pytest_splunk_addon_ui_smartx.backend_confs import ListBackendConf
+from pytest_splunk_addon_ui_smartx.components.controls.button import Button
 
 from tests.ui import constants as C
-        
+
+
 class ConfigurationPage(Page):
     """
     Page: Server page
     """
-    
+
     def __init__(
         self,
         ucc_smartx_selenium_helper=None,
@@ -32,22 +21,25 @@ class ConfigurationPage(Page):
         :param ucc_smartx_selenium_helper: smartx configuration fixture
         """
         super().__init__(ucc_smartx_selenium_helper, ucc_smartx_rest_helper, open_page)
-        
+
         self.title = Message(
-                ucc_smartx_selenium_helper.browser,
-                Selector(select='[data-test="column"] .pageTitle'),
-            )
+            ucc_smartx_selenium_helper.browser,
+            Selector(select='[data-test="column"] .pageTitle'),
+        )
         self.description = Message(
-                ucc_smartx_selenium_helper.browser,
-                Selector(select='[data-test="column"] .pageSubtitle'),
-            )
-        #self.tab_bar = 
-        
+            ucc_smartx_selenium_helper.browser,
+            Selector(select='[data-test="column"] .pageSubtitle'),
+        )
+        self.download_openapi = Button(
+            ucc_smartx_selenium_helper.browser,
+            Selector(select='[data-test="downloadButton"]'),
+        )
+
     def open(self):
         """
         Open the required page. Page(super) class opens the page by default.
         """
 
         self.browser.get(
-            f"{self.splunk_web_url}/en-US/app/{C.ADDON_NAME}"
+            f"{self.splunk_web_url}/en-US/app/{C.ADDON_NAME}/configuration"
         )
--- tests/ui/test_configuration_page.py
+++ tests/ui/test_configuration_page.py
@@ -2,7 +2,7 @@ from pytest_splunk_addon_ui_smartx.base_test import UccTester
 from tests.ui.pages.configuration_page import ConfigurationPage
 
 import pytest
-import copy
+
 
 class TestConfigurationPage(UccTester):
     @pytest.mark.execute_enterprise_cloud_true
@@ -12,10 +12,14 @@ class TestConfigurationPage(UccTester):
         self, ucc_smartx_selenium_helper, ucc_smartx_rest_helper
     ):
         """Verifies the title and description of the page"""
-        configuration_page = ConfigurationPage(ucc_smartx_selenium_helper, ucc_smartx_rest_helper)
+        configuration_page = ConfigurationPage(
+            ucc_smartx_selenium_helper, ucc_smartx_rest_helper
+        )
         self.assert_util(configuration_page.title.wait_to_display, "Configuration")
-        self.assert_util(configuration_page.description.wait_to_display, "Set up your add-on")
-        
+        self.assert_util(
+            configuration_page.description.wait_to_display, "Set up your add-on"
+        )
+
     @pytest.mark.execute_enterprise_cloud_true
     @pytest.mark.forwarder
     @pytest.mark.configuration
@@ -23,7 +27,15 @@ class TestConfigurationPage(UccTester):
         self, ucc_smartx_selenium_helper, ucc_smartx_rest_helper
     ):
         """Verifies the OpenAPI json download button"""
-        configuration_page = ConfigurationPage(ucc_smartx_selenium_helper, ucc_smartx_rest_helper)
-        download_openapi_href=configuration_page.download_openapi.container.get_attribute("href")
+        configuration_page = ConfigurationPage(
+            ucc_smartx_selenium_helper, ucc_smartx_rest_helper
+        )
+        download_openapi_href = (
+            configuration_page.download_openapi.container.get_attribute("href")
+        )
         configuration_page.download_openapi.wait_to_be_clickable()
-        self.assert_util("/en-GB/splunkd/__raw/servicesNS/nobody/Splunk_TA_UCCExample/static/openapi.json", download_openapi_href, operator="in")
+        self.assert_util(
+            "/en-GB/splunkd/__raw/servicesNS/nobody/Splunk_TA_UCCExample/static/openapi.json",
+            download_openapi_href,
+            operator="in",
+        )
