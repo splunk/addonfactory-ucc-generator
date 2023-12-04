3fdc81a3e4d5b9b222dfc7128b66bf98f474f4a1
Szymon Oleksy <soleksy@splunk.com>
2023-12-04 15:37:43 +0100

    chore: remove describe nesting

--- ui/src/components/CheckBoxComponent/CheckBoxComponent.test.tsx
+++ ui/src/components/CheckBoxComponent/CheckBoxComponent.test.tsx
@@ -3,44 +3,42 @@ import React from 'react';
 import userEvent from '@testing-library/user-event';
 import CheckBoxComponent from './CheckBoxComponent';
 
-describe('Text Component', () => {
-    const handleChange = jest.fn();
+const handleChange = jest.fn();
 
-    it('should render checkbox correctly with default value', async () => {
-        render(<CheckBoxComponent value handleChange={handleChange} field="fieldId" />);
+it('should render checkbox correctly with default value', async () => {
+    render(<CheckBoxComponent value handleChange={handleChange} field="fieldId" />);
 
-        const checkboxElement = screen.getByTestId('switch');
-        expect(checkboxElement).toBeInTheDocument();
+    const checkboxElement = screen.getByTestId('switch');
+    expect(checkboxElement).toBeInTheDocument();
 
-        const checkboxElementBtn = screen.getByTestId('button');
-        expect(checkboxElementBtn).toBeInTheDocument();
+    const checkboxElementBtn = screen.getByTestId('button');
+    expect(checkboxElementBtn).toBeInTheDocument();
 
-        expect(checkboxElementBtn).toBeChecked();
-    });
+    expect(checkboxElementBtn).toBeChecked();
+});
 
-    it('should correctly trigger callback with 0', async () => {
-        render(<CheckBoxComponent value handleChange={handleChange} field="fieldId" />);
+it('should correctly trigger callback with 0', async () => {
+    render(<CheckBoxComponent value handleChange={handleChange} field="fieldId" />);
 
-        const checkboxElementBtn = screen.getByTestId('button');
-        expect(checkboxElementBtn).toBeInTheDocument();
+    const checkboxElementBtn = screen.getByTestId('button');
+    expect(checkboxElementBtn).toBeInTheDocument();
 
-        expect(checkboxElementBtn).toBeChecked();
+    expect(checkboxElementBtn).toBeChecked();
 
-        await userEvent.click(checkboxElementBtn);
+    await userEvent.click(checkboxElementBtn);
 
-        expect(handleChange).toHaveBeenCalledWith('fieldId', 0);
-    });
+    expect(handleChange).toHaveBeenCalledWith('fieldId', 0);
+});
 
-    it('should correctly trigger callback with 1', async () => {
-        render(<CheckBoxComponent value={false} handleChange={handleChange} field="fieldId" />);
+it('should correctly trigger callback with 1', async () => {
+    render(<CheckBoxComponent value={false} handleChange={handleChange} field="fieldId" />);
 
-        const checkboxElementBtn = screen.getByTestId('button');
-        expect(checkboxElementBtn).toBeInTheDocument();
+    const checkboxElementBtn = screen.getByTestId('button');
+    expect(checkboxElementBtn).toBeInTheDocument();
 
-        expect(checkboxElementBtn).not.toBeChecked();
+    expect(checkboxElementBtn).not.toBeChecked();
 
-        await userEvent.click(checkboxElementBtn);
+    await userEvent.click(checkboxElementBtn);
 
-        expect(handleChange).toHaveBeenCalledWith('fieldId', 1);
-    });
+    expect(handleChange).toHaveBeenCalledWith('fieldId', 1);
 });
--- ui/src/components/TextAreaComponent/TextAreaComponent.test.tsx
+++ ui/src/components/TextAreaComponent/TextAreaComponent.test.tsx
@@ -3,37 +3,35 @@ import React from 'react';
 import userEvent from '@testing-library/user-event';
 import TextAreaComponent from './TextAreaComponent';
 
-describe('Text Area Component', () => {
-    const handleChange = jest.fn();
+const handleChange = jest.fn();
 
-    beforeEach(() => {
-        render(<TextAreaComponent value="test" handleChange={handleChange} field="fieldId" />);
-    });
+beforeEach(() => {
+    render(<TextAreaComponent value="test" handleChange={handleChange} field="fieldId" />);
+});
 
-    it('should render text component correctly with value', async () => {
-        const textElement = screen.getByTestId('text');
-        expect(textElement).toBeInTheDocument();
+it('should render text component correctly with value', async () => {
+    const textElement = screen.getByTestId('text');
+    expect(textElement).toBeInTheDocument();
 
-        const textBox = screen.getByTestId('textbox');
-        expect(textBox).toBeInTheDocument();
+    const textBox = screen.getByTestId('textbox');
+    expect(textBox).toBeInTheDocument();
 
-        expect(textBox).toHaveValue('test');
-    });
+    expect(textBox).toHaveValue('test');
+});
 
-    it('should trigger callback correctly after typing', async () => {
-        const textBox = screen.getByTestId('textbox');
+it('should trigger callback correctly after typing', async () => {
+    const textBox = screen.getByTestId('textbox');
 
-        await userEvent.type(textBox, 'f');
-        expect(handleChange).toHaveBeenCalledWith('fieldId', 'testf');
+    await userEvent.type(textBox, 'f');
+    expect(handleChange).toHaveBeenCalledWith('fieldId', 'testf');
 
-        await userEvent.type(textBox, 'o');
-        expect(handleChange).toHaveBeenCalledWith('fieldId', 'testo');
-    });
+    await userEvent.type(textBox, 'o');
+    expect(handleChange).toHaveBeenCalledWith('fieldId', 'testo');
+});
 
-    it('should use callback with empty string after clear', async () => {
-        const textBox = screen.getByTestId('textbox');
+it('should use callback with empty string after clear', async () => {
+    const textBox = screen.getByTestId('textbox');
 
-        await userEvent.clear(textBox);
-        expect(handleChange).toHaveBeenCalledWith('fieldId', '');
-    });
+    await userEvent.clear(textBox);
+    expect(handleChange).toHaveBeenCalledWith('fieldId', '');
 });
