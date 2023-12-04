a7dd2366fffbce68a6dfdbad8ec235cccfebd166
Szymon Oleksy <soleksy@splunk.com>
2023-12-04 16:33:51 +0100

    chore: remove nesting for DeleteModal

--- ui/src/components/DeleteModal/DeleteModal.test.tsx
+++ ui/src/components/DeleteModal/DeleteModal.test.tsx
@@ -8,60 +8,58 @@ jest.mock('immutability-helper');
 jest.mock('../../util/util');
 jest.mock('axios');
 
-describe('Delete Model Component', () => {
-    const handleClose = jest.fn();
+const handleClose = jest.fn();
 
-    const TableContext = createContext({
-        rowData: { serviceName: { stanzaName: 1 } },
-        setRowData: () => {},
-    });
+const TableContext = createContext({
+    rowData: { serviceName: { stanzaName: 1 } },
+    setRowData: () => {},
+});
 
-    beforeEach(() => {
-        render(
-            <TableContext.Provider
-                value={{ rowData: { serviceName: { stanzaName: 1 } }, setRowData: () => {} }}
-            >
-                <DeleteModal
-                    handleRequestClose={handleClose}
-                    serviceName="serviceName"
-                    stanzaName="stanzaName"
-                    page="inputs"
-                    open
-                />
-            </TableContext.Provider>
-        );
-    });
+beforeEach(() => {
+    render(
+        <TableContext.Provider
+            value={{ rowData: { serviceName: { stanzaName: 1 } }, setRowData: () => {} }}
+        >
+            <DeleteModal
+                handleRequestClose={handleClose}
+                serviceName="serviceName"
+                stanzaName="stanzaName"
+                page="inputs"
+                open
+            />
+        </TableContext.Provider>
+    );
+});
 
-    it('should render delete modal correctly', () => {
-        const deleteModal = screen.getByTestId('modal');
-        expect(deleteModal).toBeInTheDocument();
+it('should render delete modal correctly', () => {
+    const deleteModal = screen.getByTestId('modal');
+    expect(deleteModal).toBeInTheDocument();
 
-        const cancelButton = screen.getByRole('button', { name: /cancel/i });
-        expect(cancelButton).toBeInTheDocument();
+    const cancelButton = screen.getByRole('button', { name: /cancel/i });
+    expect(cancelButton).toBeInTheDocument();
 
-        const deleteButton = screen.getByRole('button', { name: /delete/i });
-        expect(deleteButton).toBeInTheDocument();
-    });
+    const deleteButton = screen.getByRole('button', { name: /delete/i });
+    expect(deleteButton).toBeInTheDocument();
+});
 
-    it('close model and callback after cancel click', async () => {
-        const cancelButton = screen.getByRole('button', { name: /cancel/i });
+it('close model and callback after cancel click', async () => {
+    const cancelButton = screen.getByRole('button', { name: /cancel/i });
 
-        await userEvent.click(cancelButton);
-        expect(handleClose).toHaveBeenCalled();
-    });
+    await userEvent.click(cancelButton);
+    expect(handleClose).toHaveBeenCalled();
+});
 
-    it('correct delete request', async () => {
-        (axios as unknown as jest.Mock).mockResolvedValue(
-            new Promise((r) =>
-                r({
-                    data: {},
-                })
-            )
-        );
+it('correct delete request', async () => {
+    (axios as unknown as jest.Mock).mockResolvedValue(
+        new Promise((r) =>
+            r({
+                data: {},
+            })
+        )
+    );
 
-        const deleteButton = screen.getByRole('button', { name: /delete/i });
-        await userEvent.click(deleteButton);
+    const deleteButton = screen.getByRole('button', { name: /delete/i });
+    await userEvent.click(deleteButton);
 
-        expect(handleClose).toHaveBeenCalled();
-    });
+    expect(handleClose).toHaveBeenCalled();
 });
