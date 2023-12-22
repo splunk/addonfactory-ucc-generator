bba7b321b7a06cc1cdefeb7bef6b797c9d63d508
soleksy-splunk <143183665+soleksy-splunk@users.noreply.github.com>
2023-12-22 11:53:45 +0100

    fix: update ui/src/components/SubDescription/SubDescription.tsx
    Co-authored-by: vtsvetkov-splunk <142901247+vtsvetkov-splunk@users.noreply.github.com>

--- ui/src/components/SubDescription/SubDescription.tsx
+++ ui/src/components/SubDescription/SubDescription.tsx
@@ -25,7 +25,7 @@ const mapTextToElements = (props: SubDescriptionProps) => {
 
             if (linkToReplace) {
                 return (
-                    <Link to={linkToReplace.link} target="_blank" rel="noreferrer">
+                    <Link to={linkToReplace.link} openInNewContext >
                         {linkToReplace.linkText}
                     </Link>
                 );
