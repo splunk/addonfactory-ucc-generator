48ac533cfd8ecc98bc02ac5ad1c1f9c704739984
sgoral <sgoral@splunk.com>
2024-04-02 18:04:48 +0200

    feat: update docs, small refactor

--- splunk_add_on_ucc_framework/dashboard.py
+++ splunk_add_on_ucc_framework/dashboard.py
@@ -17,8 +17,8 @@ import json
 import logging
 import os
 import sys
-from defusedxml import ElementTree
-from typing import Sequence, List
+
+from typing import Dict, List, Any
 
 from splunk_add_on_ucc_framework import global_config as global_config_lib
 from splunk_add_on_ucc_framework import utils
@@ -39,76 +39,99 @@ SUPPORTED_PANEL_NAMES_READABLE = ", ".join(SUPPORTED_PANEL_NAMES)
 default_definition_json_filename = {
     "overview": "overview_definition.json",
     "data_ingestion_tab": "data_ingestion_tab_definition.json",
-    "errors_tab": "errors_tab_definition.json"
+    "errors_tab": "errors_tab_definition.json",
 }
 
-data_ingestion = ('index=_internal source=/opt/splunk/var/log/splunk/license_usage.log type=Usage '
-                  '(s IN ({input_names})) | timechart span=1d sum(b) as Usage | '
-                  'eval Usage=round(Usage/1024/1024/1024,3) | rename Usage as \\"Data volume\\" ')
-data_ingestion_and_events = ('index=_internal source=/opt/splunk/var/log/splunk/license_usage.log type=Usage '
-                             '(s IN ({input_names})) | timechart span=1d sum(b) as Usage '
-                             '| eval Usage=round(Usage/1024/1024/1024,3) | rename Usage as \\"Data volume\\"'
-                             '| append [search index=_internal source=*{addon_name}* action=events_ingested '
-                             '| timechart sum(n_events) as \\"Number of events\\" ] ')
-errors_count = 'index=_internal source=*{addon_name}* ERROR | timechart span=1d count as Errors'
-events_count = ('index=_internal source=*{addon_name}* action=events_ingested | '
-                'timechart sum(n_events) as \\"Number of events\\"')
+data_ingestion = (
+    "index=_internal source=/opt/splunk/var/log/splunk/license_usage.log type=Usage "
+    "(s IN ({input_names})) | timechart span=1d sum(b) as Usage | "
+    'eval Usage=round(Usage/1024/1024/1024,3) | rename Usage as \\"Data volume\\" '
+)
+data_ingestion_and_events = (
+    "index=_internal source=/opt/splunk/var/log/splunk/license_usage.log type=Usage "
+    "(s IN ({input_names})) | timechart span=1d sum(b) as Usage "
+    '| eval Usage=round(Usage/1024/1024/1024,3) | rename Usage as \\"Data volume\\"'
+    "| append [search index=_internal source=*{addon_name}* action=events_ingested "
+    '| timechart sum(n_events) as \\"Number of events\\" ] '
+)
+errors_count = (
+    "index=_internal source=*{addon_name}* ERROR | timechart span=1d count as Errors"
+)
+events_count = (
+    "index=_internal source=*{addon_name}* action=events_ingested | "
+    'timechart sum(n_events) as \\"Number of events\\"'
+)
 
-table_sourcetype_query = ('index=_internal source=/opt/splunk/var/log/splunk/license_usage.log type=Usage '
-                          '(s IN ({input_names})) '
-                          '| stats sparkline(sum(b)), sum(b) as Bytes by st '
-                          '| join type=left st [search index = _internal source=*{addon_name}* action=events_ingested '
-                          '| stats latest(_time) AS \\"Last seen\\", sum(n_events) by sourcetype_ingested '
-                          '| rename sourcetype_ingested as st ]'
-                          '| convert ctime(\\"Last seen\\") | eval GB=round(Bytes/1024/1024/1024,3) '
-                          '| eval events=sum(b) | table st, GB, sum(n_events), sparkline(sum(b)), \\"Last seen\\" '
-                          '| rename st as \\"Source type\\", GB as \\"Data volume [GB]\\", '
-                          'sum(n_events) as \\"Number of events\\", sparkline(sum(b)) as \\"Data volume trend line\\"')
-table_source_query = ('index=_internal source=/opt/splunk/var/log/splunk/license_usage.log type=Usage '
-                      '(s IN ({input_names})) '
-                      '| stats sparkline(sum(b)), sum(b) as Bytes by s '
-                      '| join type=left s [search index = _internal source=*{addon_name}* action=events_ingested '
-                      '| stats latest(_time) AS \\"Last seen\\", sum(n_events) by modular_input_name '
-                      '| rename modular_input_name as s ]'
-                      '| convert ctime(\\"Last seen\\") | eval GB=round(Bytes/1024/1024/1024,3)| eval events=sum(b)'
-                      '| table s, GB, sum(n_events), sparkline(sum(b)), \\"Last seen\\" '
-                      '| rename s as \\"Source\\", GB as \\"Data volume [GB]\\", '
-                      'sum(n_events) as \\"Number of events\\", sparkline(sum(b)) as \\"Data volume trend line\\"')
-table_host_query = ('index=_internal source=/opt/splunk/var/log/splunk/license_usage.log type=Usage '
-                    '(s IN ({input_names})) '
-                    '| stats sparkline(sum(b)), sum(b) as Bytes by h '
-                    '| join type=left h [search index = _internal source=*{addon_name}* action=events_ingested '
-                    '| stats latest(_time) AS \\"Last seen\\", sum(n_events) by host | rename host as h ]'
-                    '| convert ctime(\\"Last seen\\") | eval GB=round(Bytes/1024/1024/1024,3)| eval events=sum(b)'
-                    '| table h, GB, sum(n_events), sparkline(sum(b)), \\"Last seen\\" '
-                    '| rename h as \\"Host\\", GB as \\"Data volume [GB]\\", '
-                    'sum(n_events) as \\"Number of events\\", sparkline(sum(b)) as \\"Data volume trend line\\"')
-table_index_query = ('index=_internal source=/opt/splunk/var/log/splunk/license_usage.log type=Usage '
-                     '(s IN ({input_names})) '
-                     '| stats sparkline(sum(b)), sum(b) as Bytes by idx '
-                     '| join type=left idx [search index = _internal source=*{addon_name}* action=events_ingested '
-                     '| stats latest(_time) AS \\"Last seen\\", sum(n_events) by event_index '
-                     '| rename event_index as idx ]'
-                     '| convert ctime(\\"Last seen\\") | eval GB=round(Bytes/1024/1024/1024,3)| eval events=sum(b)'
-                     '| table idx, GB, sum(n_events), sparkline(sum(b)), \\"Last seen\\" '
-                     '| rename idx as \\"Index\\", GB as \\"Data volume [GB]\\", '
-                     'sum(n_events) as \\"Number of events\\", sparkline(sum(b)) as \\"Data volume trend line\\"')
-table_account_query = ('index = _internal source=*{addon_name}* action=events_ingested '
-                       '| stats latest(_time) as ls, sum(n_events) by event_account | convert ctime(ls) '
-                       '| table event_account, \\"Data volume [GB]\\", sum(n_events), \\"Data volume trend line\\", ls '
-                       '| rename event_account as \\"Account\\", sum(n_events) as \\"Number of events\\", '
-                       'ls as \\"Last seen\\"')
+table_sourcetype_query = (
+    "index=_internal source=/opt/splunk/var/log/splunk/license_usage.log type=Usage "
+    "(s IN ({input_names})) "
+    "| stats sparkline(sum(b)), sum(b) as Bytes by st "
+    "| join type=left st [search index = _internal source=*{addon_name}* action=events_ingested "
+    '| stats latest(_time) AS \\"Last seen\\", sum(n_events) by sourcetype_ingested '
+    "| rename sourcetype_ingested as st ]"
+    '| convert ctime(\\"Last seen\\") | eval GB=round(Bytes/1024/1024/1024,3) '
+    '| eval events=sum(b) | table st, GB, sum(n_events), sparkline(sum(b)), \\"Last seen\\" '
+    '| rename st as \\"Source type\\", GB as \\"Data volume [GB]\\", '
+    'sum(n_events) as \\"Number of events\\", sparkline(sum(b)) as \\"Data volume trend line\\"'
+)
+table_source_query = (
+    "index=_internal source=/opt/splunk/var/log/splunk/license_usage.log type=Usage "
+    "(s IN ({input_names})) "
+    "| stats sparkline(sum(b)), sum(b) as Bytes by s "
+    "| join type=left s [search index = _internal source=*{addon_name}* action=events_ingested "
+    '| stats latest(_time) AS \\"Last seen\\", sum(n_events) by modular_input_name '
+    "| rename modular_input_name as s ]"
+    '| convert ctime(\\"Last seen\\") | eval GB=round(Bytes/1024/1024/1024,3)| eval events=sum(b)'
+    '| table s, GB, sum(n_events), sparkline(sum(b)), \\"Last seen\\" '
+    '| rename s as \\"Source\\", GB as \\"Data volume [GB]\\", '
+    'sum(n_events) as \\"Number of events\\", sparkline(sum(b)) as \\"Data volume trend line\\"'
+)
+table_host_query = (
+    "index=_internal source=/opt/splunk/var/log/splunk/license_usage.log type=Usage "
+    "(s IN ({input_names})) "
+    "| stats sparkline(sum(b)), sum(b) as Bytes by h "
+    "| join type=left h [search index = _internal source=*{addon_name}* action=events_ingested "
+    '| stats latest(_time) AS \\"Last seen\\", sum(n_events) by host | rename host as h ]'
+    '| convert ctime(\\"Last seen\\") | eval GB=round(Bytes/1024/1024/1024,3)| eval events=sum(b)'
+    '| table h, GB, sum(n_events), sparkline(sum(b)), \\"Last seen\\" '
+    '| rename h as \\"Host\\", GB as \\"Data volume [GB]\\", '
+    'sum(n_events) as \\"Number of events\\", sparkline(sum(b)) as \\"Data volume trend line\\"'
+)
+table_index_query = (
+    "index=_internal source=/opt/splunk/var/log/splunk/license_usage.log type=Usage "
+    "(s IN ({input_names})) "
+    "| stats sparkline(sum(b)), sum(b) as Bytes by idx "
+    "| join type=left idx [search index = _internal source=*{addon_name}* action=events_ingested "
+    '| stats latest(_time) AS \\"Last seen\\", sum(n_events) by event_index '
+    "| rename event_index as idx ]"
+    '| convert ctime(\\"Last seen\\") | eval GB=round(Bytes/1024/1024/1024,3)| eval events=sum(b)'
+    '| table idx, GB, sum(n_events), sparkline(sum(b)), \\"Last seen\\" '
+    '| rename idx as \\"Index\\", GB as \\"Data volume [GB]\\", '
+    'sum(n_events) as \\"Number of events\\", sparkline(sum(b)) as \\"Data volume trend line\\"'
+)
+table_account_query = (
+    "index = _internal source=*{addon_name}* action=events_ingested "
+    "| stats latest(_time) as ls, sum(n_events) by event_account | convert ctime(ls) "
+    '| table event_account, \\"Data volume [GB]\\", sum(n_events), \\"Data volume trend line\\", ls '
+    '| rename event_account as \\"Account\\", sum(n_events) as \\"Number of events\\", '
+    'ls as \\"Last seen\\"'
+)
+table_input_query = (
+    "index = _internal source=*{addon_name}* action=events_ingested "
+    "| stats latest(_time) as ls, sum(n_events) by event_input | convert ctime(ls) "
+    '| table event_input, \\"Data volume [GB]\\", sum(n_events), \\"Data volume trend line\\", ls '
+    '| rename event_input as \\"Input\\", sum(n_events) as \\"Number of events\\", '
+    'ls as \\"Last seen\\"'
+)
 
 errors_list_query = "index=_internal source=*{addon_name}* ERROR"
 
 
 def generate_dashboard_content(
-        addon_name: str,
-        input_names: List[str],
-        definition_json_name: str
+    addon_name: str, input_names: List[str], definition_json_name: str
 ) -> str:
     input_names_str = ",".join([name + "*" for name in input_names])
-    content = None
+    content = ""
 
     if definition_json_name == default_definition_json_filename["overview"]:
         content = (
@@ -131,12 +154,20 @@ def generate_dashboard_content(
                 data_ingestion=data_ingestion.format(input_names=input_names_str),
                 errors_count=errors_count.format(addon_name=addon_name.lower()),
                 events_count=events_count.format(addon_name=addon_name.lower()),
-                table_sourcetype=table_sourcetype_query.format(input_names=input_names_str,
-                                                               addon_name=addon_name.lower()),
-                table_source=table_source_query.format(input_names=input_names_str, addon_name=addon_name.lower()),
-                table_host=table_host_query.format(input_names=input_names_str, addon_name=addon_name.lower()),
-                table_index=table_index_query.format(input_names=input_names_str, addon_name=addon_name.lower()),
+                table_sourcetype=table_sourcetype_query.format(
+                    input_names=input_names_str, addon_name=addon_name.lower()
+                ),
+                table_source=table_source_query.format(
+                    input_names=input_names_str, addon_name=addon_name.lower()
+                ),
+                table_host=table_host_query.format(
+                    input_names=input_names_str, addon_name=addon_name.lower()
+                ),
+                table_index=table_index_query.format(
+                    input_names=input_names_str, addon_name=addon_name.lower()
+                ),
                 table_account=table_account_query.format(addon_name=addon_name.lower()),
+                table_input=table_input_query.format(addon_name=addon_name.lower()),
             )
         )
 
@@ -154,9 +185,9 @@ def generate_dashboard_content(
 
 
 def generate_dashboard(
-        global_config: global_config_lib.GlobalConfig,
-        addon_name: str,
-        definition_json_path: str,
+    global_config: global_config_lib.GlobalConfig,
+    addon_name: str,
+    definition_json_path: str,
 ) -> None:
     os.makedirs(os.path.normpath(definition_json_path), exist_ok=True)
 
@@ -166,8 +197,12 @@ def generate_dashboard(
 
     if PANEL_DEFAULT in panel_names:
         for definition_json_name in default_definition_json_filename.values():
-            content = generate_dashboard_content(addon_name, input_names, definition_json_name)
-            with open(os.path.join(definition_json_path, definition_json_name), "w") as file:
+            content = generate_dashboard_content(
+                addon_name, input_names, definition_json_name
+            )
+            with open(
+                os.path.join(definition_json_path, definition_json_name), "w"
+            ) as file:
                 file.write(content)
 
     if PANEL_CUSTOM in panel_names:
@@ -183,7 +218,7 @@ def generate_dashboard(
             file.write(json.dumps(custom_content))
 
 
-def get_custom_json_content(custom_dashboard_path: str) -> str:
+def get_custom_json_content(custom_dashboard_path: str) -> Dict[Any, Any]:
     custom_dashbaord = load_custom_json(custom_dashboard_path)
 
     if not custom_dashbaord:
@@ -196,16 +231,16 @@ def get_custom_json_content(custom_dashboard_path: str) -> str:
     return custom_dashbaord
 
 
-def load_custom_json(json_path: str) -> json:
+def load_custom_json(json_path: str) -> Dict[Any, Any]:
     try:
-        with open(json_path, "r") as dashboard_file:
+        with open(json_path) as dashboard_file:
             custom_dashbaord = json.load(dashboard_file)
     except FileNotFoundError:
-            logger.error(
-                f"Custom dashboard page set in globalConfig.json but "
-                f"file {json_path} not found"
-            )
-            sys.exit(1)
+        logger.error(
+            f"Custom dashboard page set in globalConfig.json but "
+            f"file {json_path} not found"
+        )
+        sys.exit(1)
     except json.decoder.JSONDecodeError:
         logger.error(f"{json_path} it's not a valid json file")
         sys.exit(1)
