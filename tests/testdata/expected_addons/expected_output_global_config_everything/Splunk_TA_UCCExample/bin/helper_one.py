2cddb3f0107f3a456b6af98ea462dadbf1bd677b
kkedziak <kkedziak@splunk.com>
2024-04-04 10:19:54 +0200

    Update helper files

--- tests/testdata/expected_addons/expected_output_global_config_everything/Splunk_TA_UCCExample/bin/helper_one.py
+++ tests/testdata/expected_addons/expected_output_global_config_everything/Splunk_TA_UCCExample/bin/helper_one.py
@@ -1,6 +1,19 @@
+import json
+
+from splunklib import modularinput as smi
+
+
 def validate_input(self, definition):
     return
 
 
 def stream_events(self, inputs, ew):
-    return
+    input_items = [{'count': len(inputs.inputs)}]
+    for input_name, input_item in inputs.inputs.items():
+        input_item['name'] = input_name
+        input_items.append(input_item)
+    event = smi.Event(
+        data=json.dumps(input_items),
+        sourcetype='example_input_one',
+    )
+    ew.write_event(event)
