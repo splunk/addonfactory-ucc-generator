791a6dc7ec6abea179c5601093a9bde86ba64ab4
soleksy-splunk <143183665+soleksy-splunk@users.noreply.github.com>
2024-04-19 14:26:42 +0200

    feat: create a playground for global config (#1144)
    https://splunk.atlassian.net/browse/ADDON-69283 
    Add playground for storybook to allow global config testing
    
    ---------
    
    Co-authored-by: srv-rr-github-token <94607705+srv-rr-github-token@users.noreply.github.com>

--- ui/src/mocks/server-response.ts
+++ ui/src/mocks/server-response.ts
@@ -9,3 +9,18 @@ export const mockServerResponse = {
     entry: [],
     messages: [],
 };
+
+export const mockServerResponseWithContent = {
+    links: {
+        create: `/servicesNS/nobody/${MOCKED_TA_NAME}/${MOCKED_TA_INPUT}/_new`,
+    },
+    updated: '2023-08-21T11:54:12+00:00',
+    entry: [
+        {
+            content: {
+                disabled: true,
+            },
+        },
+    ],
+    messages: [],
+};
