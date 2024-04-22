f5587e4551480229b868b357b52d94b198fea6f9
Wlodek Mazur <wmazur@splunk.com>
2024-04-22 14:33:27 +0200

    Fix the issue with ucc generating the output to the folder wher it is executed making single test run (in IDE) impossible

--- tests/smoke/test_ucc_build.py
+++ tests/smoke/test_ucc_build.py
@@ -103,10 +103,10 @@ def test_ucc_generate_with_config_param():
     Check if globalConfig and app.manifest contains current ucc version
     """
 
-    def check_ucc_versions():
+    def check_ucc_versions(parent_folder):
+
         global_config_path = path.join(
-            path.dirname(path.realpath(__file__)),
-            "output",
+            parent_folder,
             "Splunk_TA_UCCExample",
             "appserver",
             "static",
@@ -136,10 +136,10 @@ def test_ucc_generate_with_config_param():
         "package_global_config_everything",
         "globalConfig.json",
     )
+    with tempfile.TemporaryDirectory(prefix="ucc") as temp:
+        build.generate(source=package_folder, config_path=config_path, output_directory=temp)
 
-    build.generate(source=package_folder, config_path=config_path)
-
-    check_ucc_versions()
+        check_ucc_versions(temp)
 
 
 def test_ucc_generate_with_everything():
