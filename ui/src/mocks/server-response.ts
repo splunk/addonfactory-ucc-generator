d74a9ab0becb385be5b887463b416793bd11ac29
rohanm-crest <rohanm@splunk.com>
2024-04-26 18:00:44 +0530

    feat(storybook): add search button in input page component.

--- ui/src/mocks/server-response.ts
+++ ui/src/mocks/server-response.ts
@@ -6,7 +6,12 @@ export const mockServerResponse = {
         create: `/servicesNS/nobody/${MOCKED_TA_NAME}/${MOCKED_TA_INPUT}/_new`,
     },
     updated: '2023-08-21T11:54:12+00:00',
-    entry: [],
+    entry: [
+        {
+            name: 'name',
+            content: {},
+        },
+    ],
     messages: [],
 };
 
