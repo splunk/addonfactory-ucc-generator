3feb05bdf46f7505554bb425023538a4a3d4be96
Szymon Oleksy <soleksy@splunk.com>
2024-06-05 11:35:26 +0200

    test: add info about files

--- ui/src/pages/Dashboard/tests/mockJs.ts
+++ ui/src/pages/Dashboard/tests/mockJs.ts
@@ -2,3 +2,5 @@ Object.defineProperty(URL, 'createObjectURL', {
     writable: true,
     value: jest.fn(),
 });
+
+// needed for package import EnterpriseViewOnlyPreset from '@splunk/dashboard-presets/EnterpriseViewOnlyPreset'
