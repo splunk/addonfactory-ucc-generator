73c2a5122a0e6a2e92ee0a9486aa99849512b022
sgoral <sgoral@splunk.com>
2024-06-19 17:52:56 +0200

    refactor: change enable/disable to activate/deactivate and enabled/disabled to active/inactive

--- tests/ui/test_input_page.py
+++ tests/ui/test_input_page.py
@@ -2256,13 +2256,13 @@ class TestInputPage(UccTester):
             input_page.interact_all_prompt_entity.prompt_title.container.get_attribute(
                 "textContent"
             ).strip(),
-            "Enable all",
+            "Activate all",
         )
         self.assert_util(
             input_page.interact_all_prompt_entity.prompt_message.container.get_attribute(
                 "textContent"
             ).strip(),
-            "Do you want to enable all? It may take a while.",
+            "Do you want to activate all? It may take a while.",
         )
 
     @pytest.mark.execute_enterprise_cloud_true
@@ -2278,13 +2278,13 @@ class TestInputPage(UccTester):
             input_page.interact_all_prompt_entity.prompt_title.container.get_attribute(
                 "textContent"
             ).strip(),
-            "Disable all",
+            "Deactivate all",
         )
         self.assert_util(
             input_page.interact_all_prompt_entity.prompt_message.container.get_attribute(
                 "textContent"
             ).strip(),
-            "Do you want to disable all? It may take a while.",
+            "Do you want to deactivate all? It may take a while.",
         )
 
     @pytest.mark.execute_enterprise_cloud_true
@@ -2294,7 +2294,7 @@ class TestInputPage(UccTester):
         self, ucc_smartx_selenium_helper, ucc_smartx_rest_helper, add_input_one
     ):
         """
-        Verifies that closing the 'Enable All' prompt behaves correctly.
+        Verifies that closing the 'Activate All' prompt behaves correctly.
         """
         input_page = InputPage(ucc_smartx_selenium_helper, ucc_smartx_rest_helper)
         input_page.enable_all_inputs()
@@ -2307,7 +2307,7 @@ class TestInputPage(UccTester):
         self, ucc_smartx_selenium_helper, ucc_smartx_rest_helper, add_input_one
     ):
         """
-        Verifies that closing the 'Disable All' prompt behaves correctly.
+        Verifies that closing the 'Deactivate All' prompt behaves correctly.
         """
         input_page = InputPage(ucc_smartx_selenium_helper, ucc_smartx_rest_helper)
         input_page.disable_all_inputs()
@@ -2320,7 +2320,7 @@ class TestInputPage(UccTester):
         self, ucc_smartx_selenium_helper, ucc_smartx_rest_helper, add_input_one
     ):
         """
-        Verifies that when 'Enable All' is followed by 'Deny,' inputs remain disabled.
+        Verifies that when 'Activate All' is followed by 'Deny,' inputs remain disabled.
         """
         input_page = InputPage(ucc_smartx_selenium_helper, ucc_smartx_rest_helper)
         inputs_enabled_table = input_page.table.get_table()
@@ -2340,7 +2340,7 @@ class TestInputPage(UccTester):
         self, ucc_smartx_selenium_helper, ucc_smartx_rest_helper, add_input_one
     ):
         """
-        Verifies that when 'Disable All' is followed by 'Deny,' inputs remain enabled.
+        Verifies that when 'Deactivate All' is followed by 'Deny,' inputs remain enabled.
         """
         input_page = InputPage(ucc_smartx_selenium_helper, ucc_smartx_rest_helper)
         inputs_enabled_table = input_page.table.get_table()
@@ -2354,7 +2354,7 @@ class TestInputPage(UccTester):
     def test_inputs_disable_enable_all(
         self, ucc_smartx_selenium_helper, ucc_smartx_rest_helper, add_multiple_inputs
     ):
-        """Verifies that all inputs are disabled after clicking 'Disable all'"""
+        """Verifies that all inputs are disabled after clicking 'Deactivate all'"""
         input_page = InputPage(ucc_smartx_selenium_helper, ucc_smartx_rest_helper)
         inputs_enabled_table = input_page.table.get_table()
         inputs_disabled_table = copy.deepcopy(inputs_enabled_table)
@@ -2381,7 +2381,7 @@ class TestInputPage(UccTester):
     ):
         """
         Verifies that various types of inputs are correctly disabled and enabled
-        when using the 'Disable All' and 'Enable All' buttons.
+        when using the 'Deactivate All' and 'Activate All' buttons.
         This test covers scenarios with both Input One and Input Two added.
         """
         input_page = InputPage(ucc_smartx_selenium_helper, ucc_smartx_rest_helper)
