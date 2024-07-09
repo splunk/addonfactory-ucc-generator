7286e993d446c20654770e09e7da149b717421e2
Szymon Oleksy <soleksy@splunk.com>
2024-07-09 15:06:37 +0200

    fix: typo

--- ui/src/util/considerFalseAndTruthy.ts
+++ ui/src/util/considerFalseAndTruthy.ts
@@ -18,7 +18,7 @@ export function isTrue(value: unknown) {
  * since splunk does not oeprate on booleans and numbers, but onyl strings
  * here we use mapping to enable compering and oeprating on them
  * @param value value used for mapping
- * @returns maps truthy values into 1 and false into 0, does not midify rest
+ * @returns maps truthy values into 1 and false into 0, does not modify rest
  */
 export function getValueMapTruthyFalse<T>(value: string | T) {
     return (isFalse(value) && '0') || (isTrue(value) && '1') || value;
