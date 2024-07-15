6962c7f02476b2c7a6ac13851c34fc80d4f98bb6
Viktor Tsvetkov <142901247+vtsvetkov-splunk@users.noreply.github.com>
2024-07-15 12:41:58 +0200

    tests(utils): add tests for boolean detection utils

---
+++ ui/src/util/considerFalseAndTruthy.test.ts
@@ -0,0 +1,78 @@
+import { isFalse, isTrue, getValueMapTruthyFalse } from './considerFalseAndTruthy';
+
+describe('isFalse function', () => {
+    test.each([
+        [null, true],
+        [undefined, true],
+        ['0', true],
+        ['FALSE', true],
+        ['F', true],
+        ['N', true],
+        ['NO', true],
+        ['NONE', true],
+        ['nOnE', true],
+        ['', true],
+        [0, true],
+        [false, true],
+        ['1', false],
+        ['TRUE', false],
+        [1, false],
+        [true, false],
+        ['random string', false],
+        [{}, false],
+        [[], false],
+    ])('isFalse(%p) should return %p', (input, expected) => {
+        expect(isFalse(input)).toBe(expected);
+    });
+});
+
+describe('isTrue function', () => {
+    test.each([
+        [null, false],
+        [undefined, false],
+        ['1', true],
+        ['TRUE', true],
+        ['tRuE', true],
+        ['T', true],
+        ['Y', true],
+        ['YES', true],
+        [1, true],
+        [true, true],
+        ['0', false],
+        ['FALSE', false],
+        [0, false],
+        [false, false],
+        ['random string', false],
+        [{}, false],
+        [[], false],
+    ])('isTrue(%p) should return %p', (input, expected) => {
+        expect(isTrue(input)).toBe(expected);
+    });
+});
+
+describe('getValueMapTruthyFalse function', () => {
+    test.each([
+        [null, '0'],
+        [undefined, '0'],
+        ['0', '0'],
+        ['FALSE', '0'],
+        ['F', '0'],
+        ['N', '0'],
+        ['NO', '0'],
+        ['NONE', '0'],
+        ['', '0'],
+        ['1', '1'],
+        ['TRUE', '1'],
+        ['T', '1'],
+        ['Y', '1'],
+        ['YES', '1'],
+        ['random string', 'random string'],
+        [123, 123],
+        [true, '1'],
+        [false, '0'],
+        [{}, {}],
+        [[], '0'],
+    ])('getValueMapTruthyFalse(%p) should return %p', (input, expected) => {
+        expect(getValueMapTruthyFalse(input)).toEqual(expected);
+    });
+});
