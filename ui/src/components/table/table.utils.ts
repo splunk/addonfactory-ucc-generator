483630371c3e824a440f260d8424de91d9bbe178
Viktor Tsvetkov <142901247+vtsvetkov-splunk@users.noreply.github.com>
2024-07-23 10:08:18 +0200

    remove comment

--- ui/src/components/table/table.utils.ts
+++ ui/src/components/table/table.utils.ts
@@ -2,6 +2,5 @@ import { AcceptableFormRecord } from '../../types/components/shareableTypes';
 import { isTrue } from '../../util/considerFalseAndTruthy';
 
 export function isReadonlyRow(readonlyFieldId: string | undefined, row: AcceptableFormRecord) {
-    // !isFalse is a wider and more lose than isTrue
     return Boolean(readonlyFieldId && readonlyFieldId in row && isTrue(row[readonlyFieldId]));
 }
