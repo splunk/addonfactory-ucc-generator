9695ae27f4aa1cb0bd7239e5e1455d4c8a4fed32
hetangmodi-crest <hemodi@splunk.com>
2024-08-07 13:01:42 +0530

    fix: updated meta.conf file and template test files

--- tests/unit/test_meta_conf.py
+++ tests/unit/test_meta_conf.py
@@ -7,7 +7,6 @@ from splunk_add_on_ucc_framework import meta_conf as meta_conf_lib
 def test_create_default():
     meta_conf = meta_conf_lib.MetaConf()
     with tempfile.TemporaryDirectory() as temp_dir:
-        meta_conf.create_default()
         output_default_meta_path = os.path.join(
             temp_dir, meta_conf_lib.DEFAULT_META_FILE_NAME
         )
