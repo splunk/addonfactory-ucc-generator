282af183ec48d7fbe0f9dc0001b338a5b7180a7e
hetangmodi-crest <hemodi@splunk.com>
2024-08-12 15:08:52 +0530

    feat(xml): restructure generation of xml files in default directory

---
+++ splunk_add_on_ucc_framework/generators/file_const.py
@@ -0,0 +1,75 @@
+#
+# Copyright 2024 Splunk Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from typing import List, NamedTuple, Type, Union
+
+from splunk_add_on_ucc_framework.generators.xml_files import (
+    ConfigurationXml,
+    DashboardXml,
+    DefaultXml,
+    InputsXml,
+    RedirectXml,
+)
+
+__all__ = ["FileClass", "GEN_FILE_LIST"]
+
+
+class FileClass(NamedTuple):
+    file_name: str
+    file_class: Type[
+        Union[
+            ConfigurationXml,
+            DashboardXml,
+            DefaultXml,
+            InputsXml,
+            RedirectXml,
+        ]
+    ]
+    file_path: Union[str, List[str]]
+    file_description: str
+
+
+GEN_FILE_LIST: List[FileClass] = [
+    FileClass(
+        "configuration.xml",
+        ConfigurationXml,
+        ["default", "data", "ui", "views"],
+        ConfigurationXml.__description__,
+    ),
+    FileClass(
+        "dashboard.xml",
+        DashboardXml,
+        ["default", "data", "ui", "views"],
+        DashboardXml.__description__,
+    ),
+    FileClass(
+        "default.xml",
+        DefaultXml,
+        ["default", "data", "ui", "nav"],
+        DefaultXml.__description__,
+    ),
+    FileClass(
+        "inputs.xml",
+        InputsXml,
+        ["default", "data", "ui", "views"],
+        InputsXml.__description__,
+    ),
+    FileClass(
+        "_redirect.xml",
+        RedirectXml,
+        ["default", "data", "ui", "views"],
+        RedirectXml.__description__,
+    ),
+]
---
+++ splunk_add_on_ucc_framework/generators/xml_files/create_inputs_xml.py
@@ -0,0 +1,54 @@
+#
+# Copyright 2024 Splunk Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from splunk_add_on_ucc_framework.generators.xml_files import XMLGenerator
+from splunk_add_on_ucc_framework.global_config import GlobalConfig
+from typing import Any, Dict
+from splunk_add_on_ucc_framework import data_ui_generator
+
+
+class InputsXml(XMLGenerator):
+    __description__ = (
+        " Generates inputs.xml based on inputs configuration present in globalConfig,"
+        " in `default/data/ui/views/inputs.xml` folder"
+    )
+
+    def __init__(
+        self,
+        global_config: GlobalConfig,
+        input_dir: str,
+        output_dir: str,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(global_config, input_dir, output_dir, **kwargs)
+
+    def _set_attributes(self, **kwargs: Any) -> None:
+        if self._global_config and self._global_config.has_inputs():
+            self.inputs_xml_content = data_ui_generator.generate_views_inputs_xml(
+                self._addon_name,
+            )
+
+    def generate_xml(self) -> Dict[str, str]:
+        if self._global_config and not self._global_config.has_inputs():
+            return super().generate_xml()
+        file_path = self.get_file_output_path(
+            ["default", "data", "ui", "views", "inputs.xml"]
+        )
+        self.writer(
+            file_name="inputs.xml",
+            file_path=file_path,
+            content=self.inputs_xml_content,
+        )
+        return {"inputs.xml": file_path}
