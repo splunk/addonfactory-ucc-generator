4d1e5117ce75681a965265cfa82c913d899c8c38
rohanm-crest <rohanm@splunk.com>
2024-08-29 17:43:15 +0530

    refactor: added styling to the button in the modal footer

--- ui/src/pages/Dashboard/SpikeModal.tsx
+++ ui/src/pages/Dashboard/SpikeModal.tsx
@@ -8,11 +8,23 @@ import { StyledButton } from '../EntryPageStyle';
 const ModalWrapper = styled(Modal)`
     width: 60%;
     height: 80%;
-    margin-top: 8vh;
+    margin-top: 4vh;
 `;
 
 const StyledDiv = styled('div')`
-    display: flex;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0px 10px;
+
+    .footerBtn:first-child {
+        justify-self: start;
+    }
+
+    .footerBtn:last-child {
+        justify-self: end;
+    }
 `;
 
 export const SpikeModal = (props: {
@@ -28,6 +40,7 @@ export const SpikeModal = (props: {
         <Modal.Footer>
             <StyledDiv>
                 <StyledButton
+                    className="footerBtn"
                     appearance="default"
                     to="/app/search/search?q=search%20index%3D_internal%20source%3D"
                     target="_blank"
@@ -35,6 +48,7 @@ export const SpikeModal = (props: {
                     label="View events in search"
                 />
                 <StyledButton
+                    className="footerBtn"
                     appearance="primary"
                     onClick={props.handleRequestClose}
                     label={props.acceptBtnLabel || 'Done'}
