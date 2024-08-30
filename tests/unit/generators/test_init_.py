add8ce65e250f7f6b17d5153b2970c5736799c5c
hetangmodi-crest <hemodi@splunk.com>
2024-08-30 18:39:49 +0530

    test(unit): add file_generator test cases

---
+++ tests/unit/generators/test_init_.py
@@ -0,0 +1,20 @@
+def test___init__gen():
+    expected_classes = [
+        "FileGenerator",
+        "GEN_FILE_LIST",
+        "FileClass",
+    ]
+    expected_modules = [
+        "file_const",
+        "file_generator",
+    ]
+    import splunk_add_on_ucc_framework.generators as gen
+
+    assert gen.__all__ == expected_classes
+
+    not_allowed = ["conf_files", "xml_files", "html_files", "utils"]
+    for attrib in dir(gen):
+        if attrib.startswith("__") and attrib.endswith("__") or attrib in not_allowed:
+            # ignore the builtin modules
+            continue
+        assert attrib in expected_classes or attrib in expected_modules
