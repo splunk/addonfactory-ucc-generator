9ae382ae8a3f8a93de9c6c43d14d113312dcc2f0
Lukasz Plonka <lplonka@splunk.com>
2024-09-25 13:56:28 +0200

    Fix black

--- tests/unit/test_interval_regex.py
+++ tests/unit/test_interval_regex.py
@@ -12,24 +12,22 @@ cron_regex = re.compile(
         (True, "* * * * *"),
         (True, "1 2 3 4 5"),
         (True, "5 0 * 8 *"),
-        (True, "0 22 * * 1-5"),
-        (True, "0 0,12 1 */2 *"),
-        (True, "*/15 0 1,15 * 1-5"),
         (True, "0 12 * * 3"),
-        # (True, "* 0-20/2 * * *"), # 0-20/2 cron syntax is not supported by this regex...
+        (True, "0 22 * * 1-5"),
         (True, "* 0,2 * * *"),
         (True, "*/15 * * * *"),
-
+        (True, "*/15 0 1,15 * 1-5"),
+        (True, "0 0,12 1 */2 *"),
+        # (True, "* 0-20/2 * * *"), # 0-20/2 cron syntax is not supported by this regex...
         (True, "1"),
         (True, "-1"),
         (True, "0"),
         (True, "6371"),
-
         (False, "-2"),
         (False, "5 0 * 8 * 1"),
-        (False, "5 4 * * sun"), # This syntax (sun) is not supported
-        (False, "@weekly"), # This syntax is not supported
-     )
+        (False, "5 4 * * sun"),  # This syntax (sun) is not supported
+        (False, "@weekly"),  # This syntax is not supported
+    ),
 )
 def test_cron_regex_expression(is_valid, expression):
     match = cron_regex.fullmatch(expression)
