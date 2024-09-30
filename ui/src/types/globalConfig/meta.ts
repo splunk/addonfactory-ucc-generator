04c80e35488301522e19735fb06168160a8b0b86
Hetang Modi <62056057+hetangmodi-crest@users.noreply.github.com>
2024-09-30 17:25:19 +0530

    feat: option for configuring is_visible from globalConfig.json (#1345)
    **Issue number:** #1334 
    
    ## Summary
    Provided option for configuring is_visible in globalConfig.json
    
    ### Changes
    
    > Added the option `isVisible` in globalConfig.json, allowing users to
    configure the add-on's UI visibility directly from the globalConfig.
    
    ### User experience
    
    > Users will have additional option (`isVisible`) in meta tag of
    globalConfig.json from where they can configure the add-on's UI
    visibility.
    
    ## Checklist
    
    If your change doesn't seem to apply, please leave them unchecked.
    
    * [x] I have performed a self-review of this change
    * [x] Changes have been tested
    * [x] Changes are documented
    * [x] PR title follows [conventional commit
    semantics](https://www.conventionalcommits.org/en/v1.0.0/)

--- ui/src/types/globalConfig/meta.ts
+++ ui/src/types/globalConfig/meta.ts
@@ -11,6 +11,8 @@ export const meta = z.object({
     hideUCCVersion: z.boolean().optional(),
     checkForUpdates: z.boolean().default(true).optional(),
     searchViewDefault: z.boolean().default(false).optional(),
+    isVisible: z.boolean().default(true).optional(),
+    supportedThemes: z.array(z.string()).optional(),
 });
 
 export type meta = z.infer<typeof meta>;
