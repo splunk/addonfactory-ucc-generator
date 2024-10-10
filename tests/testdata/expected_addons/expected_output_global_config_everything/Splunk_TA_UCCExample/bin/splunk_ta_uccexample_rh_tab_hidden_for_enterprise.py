f2bcd03ea704ab89794d936f7c18b5522a54900f
Szymon Oleksy <soleksy@splunk.com>
2024-10-10 19:45:46 +0200

    test: fix add handlers to expected folder

---
+++ tests/testdata/expected_addons/expected_output_global_config_everything/Splunk_TA_UCCExample/bin/splunk_ta_uccexample_rh_tab_hidden_for_enterprise.py
@@ -0,0 +1,44 @@
+
+import import_declare_test
+
+from splunktaucclib.rest_handler.endpoint import (
+    field,
+    validator,
+    RestModel,
+    SingleModel,
+)
+from splunktaucclib.rest_handler import admin_external, util
+from splunktaucclib.rest_handler.admin_external import AdminExternalHandler
+import logging
+
+util.remove_http_proxy_env_vars()
+
+
+fields = [
+    field.RestField(
+        'text_test',
+        required=True,
+        encrypted=False,
+        default=None,
+        validator=validator.String(
+            max_len=100, 
+            min_len=1, 
+        )
+    )
+]
+model = RestModel(fields, name=None)
+
+
+endpoint = SingleModel(
+    'splunk_ta_uccexample_tab_hidden_for_enterprise',
+    model,
+    config_name='tab_hidden_for_enterprise'
+)
+
+
+if __name__ == '__main__':
+    logging.getLogger().addHandler(logging.NullHandler())
+    admin_external.handle(
+        endpoint,
+        handler=AdminExternalHandler,
+    )
