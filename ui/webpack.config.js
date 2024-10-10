5bd78d3f2f54743b4ede25e9327381900fec82c9
Viktor Tsvetkov <142901247+vtsvetkov-splunk@users.noreply.github.com>
2024-10-10 13:33:06 +0200

    chore(ui-dev-mode): ignore warning

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -51,6 +51,19 @@ module.exports = merge(baseConfig, {
     },
     devServer: {
         hot: false,
+        client: {
+            overlay: {
+                runtimeErrors: (error) => {
+                    if (
+                        error?.message ===
+                        'ResizeObserver loop completed with undelivered notifications.'
+                    ) {
+                        return false;
+                    }
+                    return true;
+                },
+            },
+        },
         proxy: [
             {
                 target: proxyTargetUrl,
