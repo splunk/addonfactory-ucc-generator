3fd0501b7a97cbdc4ce4880a2953676c5c6efcfd
soleksy-splunk <143183665+soleksy-splunk@users.noreply.github.com>
2024-12-05 18:43:55 +0100

    feat(modifyFieldsOnValue): enable field to modify itself (#1494)
    **Issue number:**
    https://splunk.atlassian.net/browse/ADDON-76687 
    ### PR Type
    
    **What kind of change does this PR introduce?**
    * [x] Feature
    * [ ] Bug Fix
    * [ ] Refactoring (no functional or API changes)
    * [ ] Documentation Update
    * [ ] Maintenance (dependency updates, CI, etc.)
    
    ## Summary
    
    ### Changes
    
    Remove verification to block field from applying modification to itself
    after value change.
    It is not a risk to allow users to modify field itself as we should be
    concerned only for value. (it is still not a problem but seems like a
    bad approach to solving problems).
    
    It also enables circular dependencies in case where modifications are
    not about value. (modifying values still should work good but seems like
    a bad practise)
    
    ### User experience
    
    User can right now modify state of field by applying changes to
    currently edited field, it allows to create ie. much more useful labels
    and help messages referencing value change.
    
    ## Checklist
    
    If an item doesn't apply to your changes, leave it unchecked.
    
    * [ ] I have performed a self-review of this change according to the
    [development
    guidelines](https://splunk.github.io/addonfactory-ucc-generator/contributing/#development-guidelines)
    * [x] Tests have been added/modified to cover the changes [(testing
    doc)](https://splunk.github.io/addonfactory-ucc-generator/contributing/#build-and-test)
    * [ ] Changes are documented
    * [x] PR title and description follows the [contributing
    principles](https://splunk.github.io/addonfactory-ucc-generator/contributing/#pull-requests)
    
    ---------
    
    Co-authored-by: srv-rr-github-token <94607705+srv-rr-github-token@users.noreply.github.com>

--- ui/src/components/BaseFormView/stories/BaseFormView.stories.tsx
+++ ui/src/components/BaseFormView/stories/BaseFormView.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
+import { fn, userEvent, within } from '@storybook/test';
+
 import BaseFormView from '../BaseFormView';
 import {
     PAGE_CONFIG_BOTH_OAUTH,
@@ -17,6 +18,8 @@ import {
     getGlobalConfigMockGroupsForConfigPage,
     getGlobalConfigMockModificationToGroupsConfig,
 } from '../BaseFormConfigMock';
+import { getGlobalConfigMockModificationToFieldItself } from '../tests/configMocks';
+import { invariant } from '../../../util/invariant';
 
 interface BaseFormStoriesProps extends BaseFormProps {
     config: GlobalConfig;
@@ -141,5 +144,33 @@ export const GroupModificationsConfig: Story = {
         handleFormSubmit: fn(),
         config: getGlobalConfigMockModificationToGroupsConfig(),
         platform: 'cloud',
+    },
+};
+
+export const FieldModifyItself: Story = {
+    args: {
+        currentServiceState: {},
+        serviceName: 'account',
+        mode: 'create' as Mode,
+        page: 'configuration',
+        stanzaName: 'unknownStanza',
+        handleFormSubmit: fn(),
+        config: getGlobalConfigMockModificationToFieldItself(),
+    },
+};
+
+export const FieldModifyItselfAfterMods: Story = {
+    args: FieldModifyItself.args,
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement);
+
+        const modifyInputText = canvas
+            .getAllByRole('textbox')
+            .find((el) => el.getAttribute('value') === 'default value');
+
+        invariant(modifyInputText, 'modification input field should be defined');
+
+        await userEvent.clear(modifyInputText);
+        await userEvent.type(modifyInputText, 'modify itself');
     },
 };
