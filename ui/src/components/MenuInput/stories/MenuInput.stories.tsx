b1c303800c23c40b1e862c31a48d0daa05e45d39
Szymon Oleksy <soleksy@splunk.com>
2025-02-05 18:29:38 +0100

    stories for hide for platform menu items services

--- ui/src/components/MenuInput/stories/MenuInput.stories.tsx
+++ ui/src/components/MenuInput/stories/MenuInput.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import React, { useState, useEffect } from 'react';
 import { z } from 'zod';
-import { fn } from '@storybook/test';
+import { fn, userEvent, within } from '@storybook/test';
 import MenuInput from '../MenuInput';
 import { setUnifiedConfig } from '../../../util/util';
 import { GlobalConfig, GlobalConfigSchema } from '../../../types/globalConfig/globalConfig';
@@ -9,9 +9,10 @@ import { getGlobalConfigMock } from '../../../mocks/globalConfigMock';
 import { invariant } from '../../../util/invariant';
 import {
     InputsPageTableSchema,
-    TableFullServiceSchema,
+    TableLessServiceSchema,
     TableSchema,
 } from '../../../types/globalConfig/pages';
+import { PageContextProvider } from '../../../context/PageContext';
 
 interface MenuInputProps {
     handleRequestOpen: (args: { serviceName: string; input?: string; groupName?: string }) => void;
@@ -31,7 +32,9 @@ function MenuInputWrapper(props: MenuInputProps) {
     }, [props.config]);
 
     return display ? (
-        <MenuInput handleRequestOpen={props.handleRequestOpen} />
+        <PageContextProvider platform="enterprise">
+            <MenuInput handleRequestOpen={props.handleRequestOpen} />
+        </PageContextProvider>
     ) : (
         <span>loading</span>
     );
@@ -46,19 +49,14 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 const table = {
+    actions: [],
     header: [
         {
-            field: 'name',
-            label: 'Input Name',
+            field: '',
+            label: '',
         },
     ],
-    moreInfo: [
-        {
-            field: 'name',
-            label: 'Name',
-        },
-    ],
-    actions: ['edit', 'delete', 'clone'],
+    customRow: {},
 } satisfies z.infer<typeof TableSchema>;
 
 const commonServices = [
@@ -66,27 +64,23 @@ const commonServices = [
         name: 'test-service-name1',
         title: 'test-service-title1',
         entity: [],
-        table,
     },
     {
         name: 'test-subservice1-name1',
         title: 'test-subservice1-title1',
         entity: [],
-        table,
     },
     {
         name: 'test-subservice1-name2',
         title: 'test-subservice1-title2',
         entity: [],
-        table,
     },
     {
         name: 'test-service-name2',
         title: 'test-service-title2',
         entity: [],
-        table,
     },
-] satisfies z.infer<typeof TableFullServiceSchema>[];
+] satisfies z.infer<typeof TableLessServiceSchema>[];
 
 const commonGroups = [
     {
@@ -117,6 +111,7 @@ export const Base: Story = {
                 inputs: {
                     title: inputs.title,
                     services: commonServices,
+                    table,
                 },
             },
         },
@@ -159,18 +154,158 @@ export const WithSubMenu: Story = {
                     ],
                     groupsMenu: commonGroups,
                     title: '',
-                    table: {
-                        actions: [],
-                        header: [
-                            {
-                                field: '',
-                                label: '',
-                            },
-                        ],
-                        customRow: {},
-                    },
+                    table,
                 },
             },
         } satisfies z.input<typeof GlobalConfigSchema>,
+    },
+};
+
+const servicesWithHideForPlatform = [
+    {
+        name: 'test-service-name1',
+        title: 'test-service-title1',
+        subTitle: 'test-service-subTitle1',
+        entity: [],
+    },
+    {
+        name: 'test-subservice1-name1',
+        title: 'test-subservice1-title1',
+        subTitle: 'test-subservice-subTitle1',
+        entity: [],
+    },
+    {
+        name: 'test-subservice1-name2',
+        title: 'test-subservice1-title2',
+        subTitle: 'test-subservice-subTitle2',
+        entity: [],
+    },
+    {
+        name: 'test-service-name2',
+        title: 'test-service-title2',
+        subTitle: 'test-service-subTitle2',
+        entity: [],
+    },
+    {
+        name: 'test-service-hide-cloud-name1',
+        title: 'test-service-hide-cloud-title1',
+        subTitle: 'test-service-hide-cloud-subTitle1',
+        entity: [],
+        hideForPlatform: 'cloud',
+    },
+    {
+        name: 'test-service-hide-cloud-name2',
+        title: 'test-service-hide-cloud-title2',
+        subTitle: 'test-service-hide-cloud-subTitle2',
+        entity: [],
+        hideForPlatform: 'cloud',
+    },
+    {
+        name: 'test-service-hide-enterprise-name1',
+        title: 'test-service-hide-enterprise-title1',
+        subTitle: 'test-service-hide-enterprise-subTitle1',
+        entity: [],
+        hideForPlatform: 'enterprise',
+    },
+    {
+        name: 'test-service-hide-enterprise-name2',
+        title: 'test-service-hide-enterprise-title2',
+        subTitle: 'test-service-hide-enterprise-subTitle2',
+        entity: [],
+        hideForPlatform: 'enterprise',
+    },
+] satisfies z.infer<typeof TableLessServiceSchema>[];
+
+const groupMenuHideForPlatofmr = [
+    {
+        groupName: 'test-service-hide-cloud-name1',
+        groupTitle: 'test-service-hide-cloud-title1',
+    },
+    {
+        groupName: 'test-service-hide-enterprise-name1',
+        groupTitle: 'test-service-hide-enterprise-title1',
+    },
+    {
+        groupName: 'test-group-name1',
+        groupTitle: 'test-group-title1',
+        groupServices: ['test-subservice1-name1', 'test-subservice1-name2'],
+    },
+    {
+        groupName: 'test-group-name2',
+        groupTitle: 'test-group-title2',
+        groupServices: ['test-service-name2', 'test-service-name1'],
+    },
+    {
+        groupName: 'test-group-hide-for-platform',
+        groupTitle: 'test-group hide for platform',
+        groupServices: ['test-service-hide-enterprise-name2', 'test-service-hide-cloud-name2'],
+    },
+] satisfies z.infer<typeof InputsPageTableSchema>['groupsMenu'];
+
+export const WithOpenedMenu: Story = {
+    args: {
+        handleRequestOpen: fn(),
+        config: {
+            ...globalConfigMock,
+            pages: {
+                ...globalConfigMock.pages,
+                inputs: {
+                    title: 'WithOpenedSubMenu',
+                    services: servicesWithHideForPlatform,
+                    groupsMenu: groupMenuHideForPlatofmr,
+                    table,
+                },
+            },
+        },
+    },
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement);
+
+        const menuDropdown = canvas.getByRole('button', { name: 'Create New Input' });
+
+        invariant(menuDropdown, 'Menu Dropdown must exist');
+
+        await userEvent.click(menuDropdown);
+    },
+};
+
+export const WithOpenedSubMenu: Story = {
+    args: {
+        handleRequestOpen: fn(),
+        config: {
+            ...globalConfigMock,
+            pages: {
+                ...globalConfigMock.pages,
+                inputs: {
+                    title: 'WithOpenedSubMenu',
+                    services: servicesWithHideForPlatform,
+                    groupsMenu: groupMenuHideForPlatofmr,
+                    table,
+                },
+            },
+        },
+    },
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement);
+
+        const user = userEvent.setup();
+
+        const menuDropdown = canvas.getByRole('button', { name: 'Create New Input' });
+
+        invariant(menuDropdown, 'Menu Dropdown must exist');
+        await user.click(menuDropdown);
+
+        const popoverId = menuDropdown.getAttribute('data-test-popover-id');
+
+        // this popover is rendered outside(next to, on the same level) of canvasElement
+        const popoverElem = document.querySelector(`#${popoverId}`);
+        invariant(popoverElem, 'Popover must exist');
+
+        const popoverCanvas = within(popoverElem as HTMLElement);
+
+        const groupMenuItem = popoverCanvas.getByRole('menuitem', {
+            name: 'test-group hide for platform',
+        });
+        await user.click(groupMenuItem);
     },
 };
