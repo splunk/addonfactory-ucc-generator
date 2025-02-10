1ef590018ef995951e3219f597ec8f576b5dfeb3
rohanm-crest <rohanm@splunk.com>
2025-02-10 18:34:51 +0530

    test: refactor test case for tablewrapper

--- ui/src/components/table/tests/TableWrapper.test.tsx
+++ ui/src/components/table/tests/TableWrapper.test.tsx
@@ -10,6 +10,7 @@ import { TableContextProvider } from '../../../context/TableContext';
 import { setUnifiedConfig } from '../../../util/util';
 import {
     getCustomModalHeaderData,
+    getSimpleConfig,
     getSimpleConfigStylePage,
     getSimpleConfigWithMapping,
     SIMPLE_NAME_TABLE_MOCK_DATA,
@@ -18,23 +19,15 @@ import {
 const handleRequestModalOpen = jest.fn();
 const handleOpenPageStyleDialog = jest.fn();
 
-it('correct render table with all elements', async () => {
-    const props = {
-        page: 'configuration',
-        serviceName: 'account',
-        handleRequestModalOpen,
-        handleOpenPageStyleDialog,
-        displayActionBtnAllRows: false,
-    } satisfies ITableWrapperProps;
+const props = {
+    page: 'configuration',
+    serviceName: 'account',
+    handleRequestModalOpen,
+    handleOpenPageStyleDialog,
+    displayActionBtnAllRows: false,
+} satisfies ITableWrapperProps;
 
-    server.use(
-        http.get('/servicesNS/nobody/-/splunk_ta_uccexample_account', () =>
-            HttpResponse.json(MockRowData)
-        )
-    );
-
-    setUnifiedConfig(SIMPLE_NAME_TABLE_MOCK_DATA);
-
+const setup = () =>
     render(
         <TableContextProvider>
             <TableWrapper {...props} />
@@ -42,64 +35,56 @@ it('correct render table with all elements', async () => {
         { wrapper: BrowserRouter }
     );
 
-    const numberOfItems = await screen.findByText('9 Items');
-    expect(numberOfItems).toBeInTheDocument();
-
-    const headerNames = ['Name', 'Actions'];
-
-    const tableHeader = screen.getAllByRole('columnheader');
-
-    expect(tableHeader.length).toEqual(headerNames.length);
-
-    headerNames.forEach((name) => {
-        const thWithName = Array.from(tableHeader).find(
-            (thElem: HTMLElement) => thElem.dataset.testLabel === name
+describe('TableWrapper - Configuration Page', () => {
+    beforeEach(() => {
+        server.use(
+            http.get('/servicesNS/nobody/-/splunk_ta_uccexample_account', () =>
+                HttpResponse.json(MockRowData)
+            )
         );
-        expect(thWithName).toBeTruthy();
+    });
+    it('correct render table with all elements', async () => {
+        setUnifiedConfig(SIMPLE_NAME_TABLE_MOCK_DATA);
+        setup();
+
+        const numberOfItems = await screen.findByText('9 Items');
+        expect(numberOfItems).toBeInTheDocument();
+
+        const headerNames = ['Name', 'Actions'];
+
+        const tableHeader = screen.getAllByRole('columnheader');
+
+        expect(tableHeader.length).toEqual(headerNames.length);
+
+        headerNames.forEach((name) => {
+            const thWithName = Array.from(tableHeader).find(
+                (thElem: HTMLElement) => thElem.dataset.testLabel === name
+            );
+            expect(thWithName).toBeTruthy();
+        });
+
+        const currentTab = SIMPLE_NAME_TABLE_MOCK_DATA.pages.configuration.tabs.find(
+            (tab) => tab.name === props.serviceName
+        );
+
+        currentTab?.entity.forEach((confEntity) =>
+            expect(screen.getByText(confEntity.label)).toBeInTheDocument()
+        );
     });
 
-    const currentTab = SIMPLE_NAME_TABLE_MOCK_DATA.pages.configuration.tabs.find(
-        (tab) => tab.name === props.serviceName
-    );
+    it('sort items after filtering', async () => {
+        setUnifiedConfig(getSimpleConfigWithMapping());
+        setup();
 
-    currentTab?.entity.forEach((confEntity) =>
-        expect(screen.getByText(confEntity.label)).toBeInTheDocument()
-    );
-});
+        const numberOfItems = await screen.findByText('Custom Text');
+        expect(numberOfItems).toBeInTheDocument();
 
-it('sort items after filtering', async () => {
-    const props = {
-        page: 'configuration',
-        serviceName: 'account',
-        handleRequestModalOpen,
-        handleOpenPageStyleDialog,
-        displayActionBtnAllRows: false,
-    } satisfies ITableWrapperProps;
+        const customHeader = document.querySelector('[data-test-label="Custom Text"]');
+        expect(customHeader).toBeInTheDocument();
 
-    server.use(
-        http.get('/servicesNS/nobody/-/splunk_ta_uccexample_account', () =>
-            HttpResponse.json(MockRowData)
-        )
-    );
-
-    setUnifiedConfig(getSimpleConfigWithMapping());
-
-    render(
-        <TableContextProvider>
-            <TableWrapper {...props} />
-        </TableContextProvider>,
-        { wrapper: BrowserRouter }
-    );
-
-    const numberOfItems = await screen.findByText('Custom Text');
-    expect(numberOfItems).toBeInTheDocument();
-
-    const customHeader = document.querySelector('[data-test-label="Custom Text"]');
-    expect(customHeader).toBeInTheDocument();
-
-    const defaultOrder = document.querySelectorAll('[data-column="custom_text"]');
-    const mappedTextDefaultOrder = Array.from(defaultOrder).map((el: Node) => el.textContent);
-    expect(mappedTextDefaultOrder).toMatchInlineSnapshot(`
+        const defaultOrder = document.querySelectorAll('[data-column="custom_text"]');
+        const mappedTextDefaultOrder = Array.from(defaultOrder).map((el: Node) => el.textContent);
+        expect(mappedTextDefaultOrder).toMatchInlineSnapshot(`
         [
           "wxyz=a",
           "xyz=ab",
@@ -113,12 +98,12 @@ it('sort items after filtering', async () => {
         ]
     `);
 
-    await userEvent.click(customHeader!);
+        await userEvent.click(customHeader!);
 
-    const allCustomTextsAsc = document.querySelectorAll('[data-column="custom_text"]');
-    const mappedTextAsc = Array.from(allCustomTextsAsc).map((el: Node) => el.textContent);
+        const allCustomTextsAsc = document.querySelectorAll('[data-column="custom_text"]');
+        const mappedTextAsc = Array.from(allCustomTextsAsc).map((el: Node) => el.textContent);
 
-    expect(mappedTextAsc).toMatchInlineSnapshot(`
+        expect(mappedTextAsc).toMatchInlineSnapshot(`
         [
           "222222",
           "aaaaa",
@@ -132,12 +117,12 @@ it('sort items after filtering', async () => {
         ]
     `);
 
-    await userEvent.click(customHeader!);
+        await userEvent.click(customHeader!);
 
-    const allCustomTextsDesc = document.querySelectorAll('[data-column="custom_text"]');
-    const mappedTextDesc = Array.from(allCustomTextsDesc).map((el: Node) => el.textContent);
+        const allCustomTextsDesc = document.querySelectorAll('[data-column="custom_text"]');
+        const mappedTextDesc = Array.from(allCustomTextsDesc).map((el: Node) => el.textContent);
 
-    expect(mappedTextDesc).toMatchInlineSnapshot(`
+        expect(mappedTextDesc).toMatchInlineSnapshot(`
         [
           "z=abcd",
           "yz=abc",
@@ -150,108 +135,97 @@ it('sort items after filtering', async () => {
           "222222",
         ]
     `);
+    });
+
+    it('Correctly render status labels with mapped values', async () => {
+        setUnifiedConfig(getSimpleConfigWithMapping());
+        setup();
+
+        const active = MockRowData.entry.find((entry) => entry.content.disabled === false);
+        const activeRow = await screen.findByLabelText(`row-${active?.name}`);
+        const statusCell = within(activeRow).getByTestId('status');
+        expect(statusCell).toHaveTextContent('Enabled Field');
+
+        const inactive = MockRowData.entry.find((entry) => entry.content.disabled === true);
+        const inActiveRow = await screen.findByLabelText(`row-${inactive?.name}`);
+        const inActiveStatusCell = within(inActiveRow).getByTestId('status');
+        expect(inActiveStatusCell).toHaveTextContent('Disabled Field');
+    });
+
+    it('Check modal correctly renders title', async () => {
+        setUnifiedConfig(getSimpleConfig());
+        setup();
+
+        // check for custom header in edit modal
+        const allEditButtons = await screen.findAllByRole('button', { name: /edit/i });
+        await userEvent.click(allEditButtons[0]);
+        expect(screen.getByRole('heading', { name: 'Update Account' })).toBeInTheDocument();
+        const cancelButton = screen.getByRole('button', { name: /cancel/i });
+        await userEvent.click(cancelButton);
+
+        // check for custom header in clone modal
+        const allCloneButtons = await screen.findAllByRole('button', { name: /clone/i });
+        await userEvent.click(allCloneButtons[0]);
+        expect(screen.getByRole('heading', { name: 'Clone Account' })).toBeInTheDocument();
+        await userEvent.click(cancelButton);
+
+        // check for custom header in delete modal
+        const allDeleteButtons = await screen.findAllByRole('button', { name: /delete/i });
+        await userEvent.click(allDeleteButtons[0]);
+        expect(screen.getByRole('heading', { name: 'Delete Confirmation' })).toBeInTheDocument();
+    });
+
+    it('Check modal correctly render custom header', async () => {
+        setUnifiedConfig(getCustomModalHeaderData());
+        setup();
+        // check for custom header in edit modal
+        const allEditButtons = await screen.findAllByRole('button', { name: /edit/i });
+        await userEvent.click(allEditButtons[0]);
+        expect(
+            screen.getByRole('heading', { name: 'Update this is custom header' })
+        ).toBeInTheDocument();
+
+        // check for custom header in clone modal
+        const allCloneButtons = await screen.findAllByRole('button', { name: /clone/i });
+        await userEvent.click(allCloneButtons[0]);
+        expect(
+            screen.getByRole('heading', { name: 'Clone this is custom header' })
+        ).toBeInTheDocument();
+        const cancelButton = screen.getByRole('button', { name: /cancel/i });
+        await userEvent.click(cancelButton);
+
+        // check for custom header in delete modal
+        const allDeleteButtons = await screen.findAllByRole('button', { name: /delete/i });
+        await userEvent.click(allDeleteButtons[0]);
+        expect(
+            screen.getByRole('heading', { name: 'Delete this is custom header' })
+        ).toBeInTheDocument();
+    });
 });
 
-it('Correctly render status labels with mapped values', async () => {
-    const props = {
-        page: 'configuration',
-        serviceName: 'account',
-        handleRequestModalOpen,
-        handleOpenPageStyleDialog,
-        displayActionBtnAllRows: false,
-    } satisfies ITableWrapperProps;
+describe('TableWrapper - Inputs Page', () => {
+    it('Check inputs count is visible', async () => {
+        const inputsProps = {
+            ...props,
+            page: 'inputs',
+            serviceName: 'example_input_one',
+        } satisfies ITableWrapperProps;
 
-    server.use(
-        http.get('/servicesNS/nobody/-/splunk_ta_uccexample_account', () =>
-            HttpResponse.json(MockRowData)
-        )
-    );
+        server.use(
+            http.get('/servicesNS/nobody/-/splunk_ta_uccexample_example_input_one', () =>
+                HttpResponse.json(MockRowDataForStatusCount)
+            )
+        );
 
-    setUnifiedConfig(getSimpleConfigWithMapping());
+        setUnifiedConfig(getSimpleConfigStylePage());
 
-    render(
-        <TableContextProvider>
-            <TableWrapper {...props} />
-        </TableContextProvider>,
-        { wrapper: BrowserRouter }
-    );
-
-    const active = MockRowData.entry.find((entry) => entry.content.disabled === false);
-    const activeRow = await screen.findByLabelText(`row-${active?.name}`);
-    const statusCell = within(activeRow).getByTestId('status');
-    expect(statusCell).toHaveTextContent('Enabled Field');
-
-    const inactive = MockRowData.entry.find((entry) => entry.content.disabled === true);
-    const inActiveRow = await screen.findByLabelText(`row-${inactive?.name}`);
-    const inActiveStatusCell = within(inActiveRow).getByTestId('status');
-    expect(inActiveStatusCell).toHaveTextContent('Disabled Field');
-});
-
-it('Check inputs count is visible', async () => {
-    const props = {
-        page: 'inputs',
-        serviceName: 'example_input_one',
-        handleRequestModalOpen,
-        handleOpenPageStyleDialog,
-        displayActionBtnAllRows: false,
-    } satisfies ITableWrapperProps;
-    server.use(
-        http.get('/servicesNS/nobody/-/splunk_ta_uccexample_example_input_one', () =>
-            HttpResponse.json(MockRowDataForStatusCount)
-        )
-    );
-
-    setUnifiedConfig(getSimpleConfigStylePage());
-
-    render(
-        <TableContextProvider>
-            <TableWrapper {...props} />
-        </TableContextProvider>,
-        { wrapper: BrowserRouter }
-    );
-    const statusCount = await screen.findByText('11 Inputs (7 of 11 enabled)');
-    expect(statusCount).toBeInTheDocument();
-});
-
-it('Check modal correctly render custom header', async () => {
-    const props = {
-        page: 'configuration',
-        serviceName: 'account',
-        handleRequestModalOpen,
-        handleOpenPageStyleDialog,
-        displayActionBtnAllRows: false,
-    } satisfies ITableWrapperProps;
-    server.use(
-        http.get('/servicesNS/nobody/-/splunk_ta_uccexample_account', () =>
-            HttpResponse.json(MockRowData)
-        )
-    );
-
-    setUnifiedConfig(getCustomModalHeaderData());
-
-    render(
-        <TableContextProvider>
-            <TableWrapper {...props} />
-        </TableContextProvider>,
-        { wrapper: BrowserRouter }
-    );
-    // check for custom header in edit modal
-    const allEditButtons = await screen.findAllByRole('button', { name: /edit/i });
-    await userEvent.click(allEditButtons[0]);
-    const getEditTitle = screen.getByTestId('title');
-    expect(getEditTitle).toHaveTextContent('Update this is custom header');
-
-    // check for custom header in clone modal
-    const allCloneButtons = await screen.findAllByRole('button', { name: /clone/i });
-    await userEvent.click(allCloneButtons[0]);
-    const getCloneTitle = screen.getByTestId('title');
-    expect(getCloneTitle).toHaveTextContent('Clone this is custom header');
-    const cancelButton = screen.getByRole('button', { name: /cancel/i });
-    await userEvent.click(cancelButton);
-
-    // check for custom header in delete modal
-    const allDeleteButtons = await screen.findAllByRole('button', { name: /delete/i });
-    await userEvent.click(allDeleteButtons[0]);
-    const getDeleteTitle = screen.getByTestId('title');
-    expect(getDeleteTitle).toHaveTextContent('Delete this is custom header');
+        render(
+            <TableContextProvider>
+                <TableWrapper {...inputsProps} />
+            </TableContextProvider>,
+            { wrapper: BrowserRouter }
+        );
+        const statusCount = await screen.findByText('11 Inputs (7 of 11 enabled)');
+        expect(statusCount).toBeInTheDocument();
+    });
 });
