305dd0ecc1067a29b8299c2d18b540d89dcdcfbb
Viktor Tsvetkov <142901247+vtsvetkov-splunk@users.noreply.github.com>
2025-03-02 09:47:12 +0100

    wip

---
+++ ui/src/styled-components-shim.js
@@ -0,0 +1,6 @@
+import styledOriginal from 'styled-components';
+const styled = styledOriginal.default || styledOriginal;
+
+// Re-export everything
+export default styled;
+export * from 'styled-components';
--- ui/vite.lib.config.ts
+++ ui/vite.lib.config.ts
@@ -2,16 +2,33 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
+import styledComponentBabelPlugin from './styled-component-babel-plugin';
 
 export default defineConfig({
     optimizeDeps: {
-        include: ['@splunk/themes'],
+        include: ['styled-components'],
     },
     plugins: [
-        react(),
+        react({
+            babel: {
+                plugins: [
+                    // styledComponentBabelPlugin,
+                    // [
+                    //     'module-resolver',
+                    //     {
+                    //         alias: {
+                    //             'styled-components': './styled-components.cjs', // <!-- cjs or js depending on your package.json `type: `
+                    //         },
+                    //     },
+                    // ],
+                ],
+            },
+        }),
         dts({
-            tsconfigPath: resolve(__dirname, 'tsconfig.lib.json'),
+            tsconfigPath: 'tsconfig.app.json',
+            outDir: 'dist/lib',
             rollupTypes: true,
+            // include: ['src/types/*.d.ts', 'src/publicApi.ts'],
         }),
     ],
     base: '',
@@ -19,24 +36,30 @@ export default defineConfig({
         copyPublicDir: false,
         lib: {
             entry: resolve(__dirname, 'src/publicApi.ts'),
-            name: 'ucc-ui',
             formats: ['es'],
-            fileName: 'ucc-ui',
         },
-        minify: false,
         commonjsOptions: {
             transformMixedEsModules: true,
             requireReturnsDefault: 'auto',
         },
+        minify: false,
         emptyOutDir: true,
         rollupOptions: {
             // externalize deps that shouldn't be bundled into your library
-            external: ['react'],
+            external: ['react', 'react-dom', 'styled-components'],
             output: {
+                format: 'esm',
                 dir: 'dist/lib',
                 entryFileNames: 'index.js',
                 chunkFileNames: '[name]-[hash].js',
+                // https://github.com/styled-components/styled-components/issues/3700
+                interop: 'auto',
             },
+        },
+    },
+    resolve: {
+        alias: {
+            'styled-components': 'src/styled-components-shim.js',
         },
     },
 });
