02c822bbafcb2a6796f6e94d422a42de6aaaa9d3
Viktor Tsvetkov <142901247+vtsvetkov-splunk@users.noreply.github.com>
2025-03-04 16:59:19 +0100

    cleaning up (it is still working though 🎉)

--- ui/vite.lib.config.ts
+++ ui/vite.lib.config.ts
@@ -7,40 +7,30 @@ import { peerDependencies } from './package.json';
 const modulesNotToBundle = Object.keys(peerDependencies);
 
 export default defineConfig({
-    optimizeDeps: {
-        include: ['styled-components'],
-    },
     plugins: [
-        react({
-            jsxRuntime: 'classic',
-        }),
+        react(),
         dts({
             tsconfigPath: 'tsconfig.app.json',
             outDir: 'dist/lib',
             rollupTypes: true,
         }),
     ],
-    base: '',
     build: {
         copyPublicDir: false,
         lib: {
             entry: resolve(__dirname, 'src/publicApi.ts'),
-            name: 'ucc-ui',
             formats: ['es'],
             fileName: 'index',
         },
-        minify: false,
         emptyOutDir: true,
         commonjsOptions: {
-            transformMixedEsModules: true,
-            esmExternals: (id) => id === 'styled-components',
+            esmExternals: ['styled-components'],
         },
         rollupOptions: {
             external: modulesNotToBundle,
             output: {
                 format: 'module',
                 dir: 'dist/lib',
-                interop: 'compat',
             },
         },
     },
