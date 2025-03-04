859682c25dc2d88150a682e66e6cda5e37f4478b
Viktor Tsvetkov <142901247+vtsvetkov-splunk@users.noreply.github.com>
2025-03-04 14:35:44 +0100

    IT IS WORKING DON'T BREATHE 🫣

--- ui/src/styled-components-shim.js
+++
@@ -1,6 +0,0 @@
-import styledOriginal from 'styled-components';
-const styled = styledOriginal.default || styledOriginal;
-
-// Re-export everything
-export default styled;
-export * from 'styled-components';
