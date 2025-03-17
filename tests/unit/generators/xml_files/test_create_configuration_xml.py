24801eb7fefa6f8529a16433fcc18d965c5796f3
hetangmodi-crest <hemodi@splunk.com>
2025-03-17 13:53:26 +0530

    refactor(unit): centralize fixtures in conftest

--- tests/unit/generators/xml_files/test_create_configuration_xml.py
+++ tests/unit/generators/xml_files/test_create_configuration_xml.py
@@ -1,38 +1,5 @@
-from pytest import fixture
 from unittest.mock import patch, MagicMock
 from splunk_add_on_ucc_framework.generators.xml_files import ConfigurationXml
-from splunk_add_on_ucc_framework.global_config import GlobalConfig
-from tests.unit.helpers import get_testdata_file_path
-
-
-@fixture
-def global_config():
-    return GlobalConfig(get_testdata_file_path("valid_config.json"))
-
-
-@fixture
-def global_config_without_configuration():
-    return GlobalConfig(get_testdata_file_path("valid_config_no_configuration.json"))
-
-
-@fixture
-def input_dir(tmp_path):
-    return str(tmp_path / "input_dir")
-
-
-@fixture
-def output_dir(tmp_path):
-    return str(tmp_path / "output_dir")
-
-
-@fixture
-def ucc_dir(tmp_path):
-    return str(tmp_path / "ucc_dir")
-
-
-@fixture
-def ta_name():
-    return "test_addon"
 
 
 @patch(
@@ -40,12 +7,12 @@ def ta_name():
     return_value="<xml></xml>",
 )
 def test_set_attributes(
-    mock_generate_xml, global_config, input_dir, output_dir, ucc_dir, ta_name
+    mock_generate_xml, global_config_all_json, input_dir, output_dir, ucc_dir, ta_name
 ):
     config_xml = ConfigurationXml(
-        global_config=global_config,
-        input_dir=input_dir,
-        output_dir=output_dir,
+        global_config_all_json,
+        input_dir,
+        output_dir,
         ucc_dir=ucc_dir,
         addon_name=ta_name,
     )
@@ -60,9 +27,9 @@ def test_set_attributes_without_configuration(
     ta_name,
 ):
     config_xml = ConfigurationXml(
-        global_config=global_config_without_configuration,
-        input_dir=input_dir,
-        output_dir=output_dir,
+        global_config_without_configuration,
+        input_dir,
+        output_dir,
         ucc_dir=ucc_dir,
         addon_name=ta_name,
     )
@@ -82,9 +49,9 @@ def test_generate_xml_without_configuration(
     ta_name,
 ):
     configuration_xml = ConfigurationXml(
-        global_config=global_config_without_configuration,
-        input_dir=input_dir,
-        output_dir=output_dir,
+        global_config_without_configuration,
+        input_dir,
+        output_dir,
         ucc_dir=ucc_dir,
         addon_name=ta_name,
     )
@@ -107,16 +74,16 @@ def test_generate_xml_without_configuration(
 def test_generate_xml(
     mock_op_path,
     mock_set_attributes,
-    global_config,
+    global_config_all_json,
     input_dir,
     output_dir,
     ucc_dir,
     ta_name,
 ):
     config_xml = ConfigurationXml(
-        global_config=global_config,
-        input_dir=input_dir,
-        output_dir=output_dir,
+        global_config_all_json,
+        input_dir,
+        output_dir,
         ucc_dir=ucc_dir,
         addon_name=ta_name,
     )
