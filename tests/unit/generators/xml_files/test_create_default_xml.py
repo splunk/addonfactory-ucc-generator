7878f2240f202502039553659774d9bd6f28a5bb
kkedziak <kkedziak@splunk.com>
2025-03-18 11:09:29 +0100

    Merge

--- tests/unit/generators/xml_files/test_create_default_xml.py
+++ tests/unit/generators/xml_files/test_create_default_xml.py
@@ -1,38 +1,6 @@
 from pytest import fixture, raises
 from unittest.mock import patch, MagicMock
 from splunk_add_on_ucc_framework.generators.xml_files import DefaultXml
-from splunk_add_on_ucc_framework.global_config import GlobalConfig
-from tests.unit.helpers import get_testdata_file_path
-
-
-@fixture
-def global_config():
-    return GlobalConfig(get_testdata_file_path("valid_config.json"))
-
-
-@fixture
-def global_config_for_conf_only_TA():
-    return GlobalConfig(get_testdata_file_path("valid_global_config_conf_only_TA.json"))
-
-
-@fixture
-def input_dir(tmp_path):
-    return str(tmp_path / "input_dir")
-
-
-@fixture
-def output_dir(tmp_path):
-    return str(tmp_path / "output_dir")
-
-
-@fixture
-def ucc_dir(tmp_path):
-    return str(tmp_path / "ucc_dir")
-
-
-@fixture
-def ta_name():
-    return "test_addon"
 
 
 @fixture
@@ -41,11 +9,11 @@ def wrong_ta_name():
 
 
 def test_set_attribute_with_error(
-    global_config, input_dir, output_dir, wrong_ta_name, ucc_dir
+    global_config_all_json, input_dir, output_dir, wrong_ta_name, ucc_dir
 ):
     with raises(ValueError):
         DefaultXml(
-            global_config,
+            global_config_all_json,
             input_dir,
             output_dir,
             ucc_dir=ucc_dir,
@@ -58,12 +26,17 @@ def test_set_attribute_with_error(
     return_value="<xml></xml>",
 )
 def test_set_attribute(
-    mock_data_ui_generator, global_config, input_dir, output_dir, ucc_dir, ta_name
+    mock_data_ui_generator,
+    global_config_all_json,
+    input_dir,
+    output_dir,
+    ucc_dir,
+    ta_name,
 ):
     default_xml = DefaultXml(
-        global_config,
-        input_dir=input_dir,
-        output_dir=output_dir,
+        global_config_all_json,
+        input_dir,
+        output_dir,
         ucc_dir=ucc_dir,
         addon_name=ta_name,
     )
@@ -76,8 +49,8 @@ def test_set_attribute_with_no_pages(
 ):
     default_xml = DefaultXml(
         global_config_for_conf_only_TA,
-        input_dir=input_dir,
-        output_dir=output_dir,
+        input_dir,
+        output_dir,
         ucc_dir=ucc_dir,
         addon_name=ta_name,
     )
@@ -95,16 +68,16 @@ def test_set_attribute_with_no_pages(
 def test_generate_xml(
     mock_op_path,
     mock_set_attributes,
-    global_config,
+    global_config_all_json,
     input_dir,
     output_dir,
     ucc_dir,
     ta_name,
 ):
     config_xml = DefaultXml(
-        global_config=global_config,
-        input_dir=input_dir,
-        output_dir=output_dir,
+        global_config_all_json,
+        input_dir,
+        output_dir,
         ucc_dir=ucc_dir,
         addon_name=ta_name,
     )
