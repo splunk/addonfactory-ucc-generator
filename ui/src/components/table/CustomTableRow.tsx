b805b6895d4ec25ee03c323e3c7490e7eae89f27
Szymon Oleksy <soleksy@splunk.com>
2025-04-08 00:41:02 +0200

    add custom cell class

--- ui/src/components/table/CustomTableRow.tsx
+++ ui/src/components/table/CustomTableRow.tsx
@@ -75,7 +75,7 @@ function CustomTableRow(props: CustomTableRowProps) {
             row: customRow,
             fileName: header.customCell.src,
             type: header.customCell.type,
-            moreInfo: [], // this is required but not shared with the custom cell
+            moreInfo: [], // this is required but should be deleted after cleaning
         });
 
     const rowActionsPrimaryButton = useCallback(
