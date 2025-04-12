929d2025f00c526285322a74d6212f9241fd142f
Szymon Oleksy <soleksy@splunk.com>
2025-04-12 15:35:29 +0200

    simple vite config for ucc gen

--- ui/cli/vite.config_ucc-gen-ui.ts
+++ ui/cli/vite.config_ucc-gen-ui.ts
@@ -14,51 +14,51 @@ const uiDir = __dirname.split('/node_modules')[0];
 
 const DEBUG = process.env.NODE_ENV !== 'production';
 
-const proxyTargetUrl = 'http://localhost:8000';
+// const proxyTargetUrl = 'http://localhost:8000';
 
-const jsAssetsRegex = /.+\/app\/.+\/js\/build\/custom(\/.+(js(.map)?))/;
+// const jsAssetsRegex = /.+\/app\/.+\/js\/build\/custom(\/.+(js(.map)?))/;
 
-function isItStaticAsset(url: string): boolean {
-    return jsAssetsRegex.test(url);
-}
+// function isItStaticAsset(url: string): boolean {
+//     return false;
+// }
 
 const entryDir = join(uiDir, 'src/ucc-ui-extensions');
 
-/**
- * It looks for all index files in the given directory.
- * @param {string} dir
- * @return {string[]}
- */
-function getAllIndexFiles(dir: string): string[] {
-    let results: string[] = [];
-    const list = readdirSync(dir);
-    list.forEach((file) => {
-        const filePath = join(dir, file);
-        const stat = statSync(filePath);
-        if (stat && stat.isDirectory()) {
-            results = results.concat(getAllIndexFiles(filePath));
-        } else if (file === 'index.ts' || file === 'index.tsx') {
-            results.push(filePath);
-        }
-    });
-    return results;
-}
+// /**
+//  * It looks for all index files in the given directory.
+//  * @param {string} dir
+//  * @return {string[]}
+//  */
+// function getAllIndexFiles(dir: string): string[] {
+//     let results: string[] = [];
+//     const list = readdirSync(dir);
+//     list.forEach((file) => {
+//         const filePath = join(dir, file);
+//         const stat = statSync(filePath);
+//         if (stat && stat.isDirectory()) {
+//             results = results.concat(getAllIndexFiles(filePath));
+//         } else if (file === 'index.ts' || file === 'index.tsx') {
+//             results.push(filePath);
+//         }
+//     });
+//     return results;
+// }
 
-const entryFiles = getAllIndexFiles(entryDir);
+// const entryFiles = getAllIndexFiles(entryDir);
 
-if (entryFiles.length === 0) {
-    throw new Error(
-        'No entry files found. Make sure the entryDir is correct and there are index files in some directory.'
-    );
-}
+// if (entryFiles.length === 0) {
+//     throw new Error(
+//         'No entry files found. Make sure the entryDir is correct and there are index files in some directory.'
+//     );
+// }
 
 // Create input object for multiple entry points
 const input: Record<string, string> = {};
 
-entryFiles.forEach((file) => {
-    const entryName = relative(entryDir, dirname(file));
-    input[entryName] = file;
-});
+// entryFiles.forEach((file) => {
+//     const entryName = relative(entryDir, dirname(file));
+//     input[entryName] = file;
+// });
 
 // Add the UCC UI entry point
 input['ucc-ui'] = join(uiDir, 'src/ucc-ui.ts');
@@ -71,7 +71,7 @@ export default defineConfig({
         outDir: outputDir,
         sourcemap: false,
         assetsDir: '',
-        minify: false, // to be removed befire release
+        minify: true,
         rollupOptions: {
             input,
             output: {
@@ -80,11 +80,13 @@ export default defineConfig({
                     if (chunkInfo.name === 'ucc-ui') {
                         return 'entry_page.js';
                     }
-                    return Object.keys(input).includes(chunkInfo.name)
-                        ? 'custom/[name].js'
-                        : DEBUG
-                        ? '[name].js'
-                        : '[name].[hash].js';
+                    return DEBUG ? '[name].js' : '[name].[hash].js';
+                    // return Object.keys(input).includes(chunkInfo.name)
+                    //     ? 'custom/[name].js'
+                    //     :
+                    //      DEBUG
+                    //     ? '[name].js'
+                    //     : '[name].[hash].js';
                 },
             },
         },
@@ -116,34 +118,34 @@ export default defineConfig({
                 });
             },
         },
-        {
-            name: 'url-rewrite-middleware',
-            configureServer(server) {
-                server.middlewares.use((req, res, next) => {
-                    if (req.url && isItStaticAsset(req.url)) {
-                        req.url = req.url.replace(jsAssetsRegex, '$1');
-                    }
-                    next();
-                });
-            },
-        },
+        // {
+        //     name: 'url-rewrite-middleware',
+        //     configureServer(server) {
+        //         server.middlewares.use((req, res, next) => {
+        //             // if (req.url && isItStaticAsset(req.url)) {
+        //             //     req.url = req.url.replace(jsAssetsRegex, '$1');
+        //             // }
+        //             next();
+        //         });
+        //     },
+        // },
     ],
-    server: {
-        proxy: {
-            // Configure proxy based on URL pattern
-            '/': {
-                target: proxyTargetUrl,
-                changeOrigin: true,
-                bypass: ({ url }) => {
-                    // Don't proxy static assets
-                    if (url && isItStaticAsset(url)) {
-                        return url;
-                    }
-                    return null; // Proxy everything else
-                },
-            },
-        },
-    },
+    // server: {
+    //     proxy: {
+    //         // Configure proxy based on URL pattern
+    //         '/': {
+    //             target: proxyTargetUrl,
+    //             changeOrigin: true,
+    //             bypass: ({ url }) => {
+    //                 // Don't proxy static assets
+    //                 // if (url && isItStaticAsset(url)) {
+    //                 //     return url;
+    //                 // }
+    //                 return null; // Proxy everything else
+    //             },
+    //         },
+    //     },
+    // },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
     },
