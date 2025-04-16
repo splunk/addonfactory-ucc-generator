6d8b52d3254f14fa1d142d40beb2c9c59d7fc65c
rohanm-crest <rohanm@splunk.com>
2025-04-16 16:02:57 +0530

    test: ci trigger

--- ui/src/components/CheckboxTree/CheckboxTree.test.tsx
+++ ui/src/components/CheckboxTree/CheckboxTree.test.tsx
@@ -84,6 +84,7 @@ describe('CheckboxTree Component', () => {
         expect(screen.getByLabelText('Row under Group 1')).toBeInTheDocument();
         expect(screen.getByLabelText('First row under Group 3')).toBeInTheDocument();
         expect(screen.getByLabelText('First row under Group 3')).toBeInTheDocument();
+        expect(screen.getByLabelText('First row under Group 3')).toBeInTheDocument();
         expect(screen.getByLabelText('Second row under Group 3')).toBeInTheDocument();
     });
 
