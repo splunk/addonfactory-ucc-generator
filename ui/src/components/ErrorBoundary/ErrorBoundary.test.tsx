02c1983112fa4f13cfee437a9e8208e71de37e8f
Szymon Oleksy <soleksy@splunk.com>
2025-04-24 22:48:41 +0200

    fix lint

--- ui/src/components/ErrorBoundary/ErrorBoundary.test.tsx
+++ ui/src/components/ErrorBoundary/ErrorBoundary.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { act, render, screen, within } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import ErrorBoundary from './ErrorBoundary';
 import { consoleError } from '../../../jest.setup';
