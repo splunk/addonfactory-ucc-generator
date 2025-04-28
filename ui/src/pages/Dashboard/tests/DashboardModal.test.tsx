bb0f038c0f6a7ab60d2672462b7138f44a1a0775
Szymon Oleksy <soleksy@splunk.com>
2025-04-28 21:11:40 +0200

    dashboard 28.1.0

--- ui/src/pages/Dashboard/tests/DashboardModal.test.tsx
+++ ui/src/pages/Dashboard/tests/DashboardModal.test.tsx
@@ -17,22 +17,24 @@ const handleSelect = jest.fn();
 
 describe('render data ingestion modal inputs', () => {
     it('renders with all default modal dashboard elements', async () => {
-        const dimensions = {
-            x: 0,
-            y: 0,
-            width: 100,
-            height: 100,
-            top: 0,
-            left: 0,
-            right: 100,
-            bottom: 100,
-        };
-        const mockBounding = jest.fn(() => ({
-            ...dimensions,
-            toJSON: jest.fn(() => dimensions),
-        }));
+        // needed for dashabord freater than 28.1.0
+        // commented as for now using the version 28.1.0
+        // const dimensions = {
+        //     x: 0,
+        //     y: 0,
+        //     width: 100,
+        //     height: 100,
+        //     top: 0,
+        //     left: 0,
+        //     right: 100,
+        //     bottom: 100,
+        // };
+        // const mockBounding = jest.fn(() => ({
+        //     ...dimensions,
+        //     toJSON: jest.fn(() => dimensions),
+        // }));
 
-        Element.prototype.getBoundingClientRect = mockBounding;
+        // Element.prototype.getBoundingClientRect = mockBounding;
         consoleError.mockImplementation(() => {});
         server.use(
             http.get('/custom/data_ingestion_modal_definition.json', () =>
@@ -86,6 +88,5 @@ describe('render data ingestion modal inputs', () => {
                 expect(screen.getByTestId(id)).toBeInTheDocument();
             });
         });
-        mockBounding.mockClear();
     });
 });
