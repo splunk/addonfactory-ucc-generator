96ac050d697ab0f754754100ddd16194e3873263
Szymon Oleksy <soleksy@splunk.com>
2025-04-28 19:08:21 +0200

    add polifills

--- ui/jest.polyfills.ts
+++ ui/jest.polyfills.ts
@@ -14,3 +14,18 @@
 HTMLCanvasElement.prototype.getContext = jest.fn();
 
 // --- END of unnecessary polyfills
+
+// required by @splunk/dashboards
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    })),
+});
