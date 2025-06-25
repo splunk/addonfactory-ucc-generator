a99f5ccb6958f969c946da164ca1cb64805bf422
hetangmodi-crest <hemodi@splunk.com>
2025-06-25 13:04:14 +0530

    chore: trying stdlib to make the API calls

--- tests/testdata/expected_addons/expected_output_global_config_everything/client-test.py
+++ tests/testdata/expected_addons/expected_output_global_config_everything/client-test.py
@@ -1,34 +1,58 @@
 import openapi_client
 from openapi_client.api.default_api import DefaultApi
+import urllib.request
+import base64
+import json
+import ssl
 
-configuration = openapi_client.Configuration(
-    host = "https://localhost:8089/servicesNS/-/Splunk_TA_UCCExample",
-    username = "admin",
-    password = "Chang3d!",
-)
 
-configuration.verify_ssl = False
+# configuration = openapi_client.Configuration(
+#     host = "https://localhost:8089/servicesNS/-/Splunk_TA_UCCExample",
+#     username = "admin",
+#     password = "Chang3d!",
+# )
 
-with openapi_client.ApiClient(configuration) as api_client:
-    api_instance = DefaultApi(api_client)
+# configuration.verify_ssl = False
 
-    output_mode = 'json'
 
-    # List accounts (should be 0)
-    api_response = api_instance.splunk_ta_uccexample_account_get(output_mode)
-    assert not api_response.entry
+# Replace with your actual values
+url = "https://localhost:8089/servicesNS/-/Splunk_TA_UCCExample/splunk_ta_uccexample_account"
 
-    # Add account some_name
-    api_instance.splunk_ta_uccexample_account_post(output_mode, name = "some_name", account_multiple_select="aaa", account_radio="bbb", custom_endpoint="login.example.com")
+credentials = "admin:Chang3d!"
+encoded_credentials = base64.b64encode(credentials.encode("utf-8")).decode("utf-8")
+ssl_context = ssl._create_unverified_context()
 
-    # List accounts (should be 1)
-    api_response = api_instance.splunk_ta_uccexample_account_get(output_mode)
-    assert len(api_response.entry) == 1
-    assert api_response.entry[0].name == "some_name"
+# Create request with Authorization header
+req = urllib.request.Request(url)
+req.add_header("Authorization", f"Basic {encoded_credentials}")
+req.add_header("Accept", "application/json")
 
-    # Delete account
-    api_instance.splunk_ta_uccexample_account_name_delete("some_name", output_mode)
+with urllib.request.urlopen(req, context=ssl_context, timeout=60) as response:
+    response_data = response.read()
+    data = json.loads(response_data.decode('utf-8'))
+    print(data)
+    assert not data["entry"]
 
-    # List accounts (should be 0)
-    api_response = api_instance.splunk_ta_uccexample_account_get(output_mode)
-    assert not api_response.entry
+# with openapi_client.ApiClient(configuration) as api_client:
+#     api_instance = DefaultApi(api_client)
+
+#     output_mode = 'json'
+
+#     # List accounts (should be 0)
+#     api_response = api_instance.splunk_ta_uccexample_account_get(output_mode)
+#     assert not api_response.entry
+
+#     # Add account some_name
+#     api_instance.splunk_ta_uccexample_account_post(output_mode, name = "some_name", account_multiple_select="aaa", account_radio="bbb", custom_endpoint="login.example.com")
+
+#     # List accounts (should be 1)
+#     api_response = api_instance.splunk_ta_uccexample_account_get(output_mode)
+#     assert len(api_response.entry) == 1
+#     assert api_response.entry[0].name == "some_name"
+
+#     # Delete account
+#     api_instance.splunk_ta_uccexample_account_name_delete("some_name", output_mode)
+
+#     # List accounts (should be 0)
+#     api_response = api_instance.splunk_ta_uccexample_account_get(output_mode)
+#     assert not api_response.entry
