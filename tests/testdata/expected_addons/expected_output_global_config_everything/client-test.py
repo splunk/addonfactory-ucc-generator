640694f68f97380d3d6474769b9d02a003c06565
hetangmodi-crest <hemodi@splunk.com>
2025-06-25 13:39:16 +0530

    chore: list all apps and make API call

--- tests/testdata/expected_addons/expected_output_global_config_everything/client-test.py
+++ tests/testdata/expected_addons/expected_output_global_config_everything/client-test.py
@@ -5,34 +5,48 @@ import base64
 import json
 import ssl
 
-
+host = "https://localhost:8089/servicesNS/-/"
+addon_name = "Splunk_TA_UCCExample"
+username = "admin"
+password = "Chang3d!"
+query_param = "output_mode=json"
 # configuration = openapi_client.Configuration(
-#     host = "https://localhost:8089/servicesNS/-/Splunk_TA_UCCExample",
-#     username = "admin",
-#     password = "Chang3d!",
+#     host = host,
+#     username = username,
+#     password = password,
 # )
 
 # configuration.verify_ssl = False
 
+def make_request(url: str):
+    encoded_credentials = base64.b64encode(f"{username}:{password}".encode("utf-8")).decode("utf-8")
+    ssl_context = ssl._create_unverified_context()
 
-# Replace with your actual values
-url = "https://localhost:8089/servicesNS/-/Splunk_TA_UCCExample/splunk_ta_uccexample_account"
+    req = urllib.request.Request(url)
+    req.add_header("Authorization", f"Basic {encoded_credentials}")
+    req.add_header("Accept", "application/json")
 
-credentials = "admin:Chang3d!"
-encoded_credentials = base64.b64encode(credentials.encode("utf-8")).decode("utf-8")
-ssl_context = ssl._create_unverified_context()
+    with urllib.request.urlopen(req, context=ssl_context, timeout=60) as response:
+        response_data = response.read()
+        data = json.loads(response_data.decode('utf-8'))
+        return data
 
-# Create request with Authorization header
-req = urllib.request.Request(url)
-req.add_header("Authorization", f"Basic {encoded_credentials}")
-req.add_header("Accept", "application/json")
+def list_all_apps():
+    response = make_request(url= f"{host}?{query_param}")
+    exp_addon_installed = False
+    for app in response["entry"]:
+        print(f"App Installed: {app["name"]}")
+        exp_addon_installed = True if app["name"] == addon_name else exp_addon_installed
+    
+    if exp_addon_installed:
+        perform_addon_account_crud()
 
-with urllib.request.urlopen(req, context=ssl_context, timeout=60) as response:
-    response_data = response.read()
-    data = json.loads(response_data.decode('utf-8'))
-    print(data)
-    assert not data["entry"]
+def perform_addon_account_crud():
+    response = make_request(url= f"{host}{addon_name}/splunk_ta_uccexample_account?{query_param}")
+    print(f"GET response for accounts endpoint: {response}")
+    assert not response["entry"]
 
+list_all_apps()
 # with openapi_client.ApiClient(configuration) as api_client:
 #     api_instance = DefaultApi(api_client)
 
