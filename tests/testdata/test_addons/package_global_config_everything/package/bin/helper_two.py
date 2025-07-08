7135850d72b4452aacda1c9b99af589e6308d0ae
sgoral <sgoral@splunk.com>
2025-07-08 20:30:51 +0200

    test: test do not merge

--- tests/testdata/test_addons/package_global_config_everything/package/bin/helper_two.py
+++ tests/testdata/test_addons/package_global_config_everything/package/bin/helper_two.py
@@ -3,11 +3,11 @@ import json
 from splunklib import modularinput as smi
 
 
-def validate_input(definition: smi.ValidationDefinition):
+def validate_input(self, definition: smi.ValidationDefinition):
     return
 
 
-def stream_events(inputs: smi.InputDefinition, ew: smi.EventWriter):
+def stream_events(self, inputs: smi.InputDefinition, ew: smi.EventWriter):
     # INPUT 2
     input_items = [{'count': len(inputs.inputs)}]
     for input_name, input_item in inputs.inputs.items():
