069ba5631a120b570de3116d27a6d001467b9554
kkedziak-splunk <kkedziak@splunk.com>
2025-12-29 22:20:09 +0100

    Change imports

--- ui/src/components/MarkdownMessage/stories/MarkdownMessage.stories.ts
+++ ui/src/components/MarkdownMessage/stories/MarkdownMessage.stories.ts
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import MarkdownMessage from '../MarkdownMessage';
 
 const meta = {
--- ui/src/pages/Dashboard/stories/DashboardPage.stories.tsx
+++ ui/src/pages/Dashboard/stories/DashboardPage.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import React from 'react';
 import { waitForElementToBeRemoved, within } from '@storybook/test';
 import DashboardPage from '../DashboardPage';
